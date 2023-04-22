5765310d4cfc4bbe4d87b56909c4363238254ea0
SergeiLebedev0412 <130623973+SergeiLebedev0412@users.noreply.github.com>
2023-04-22 19:55:23 -0400

    add testAddDescription (#936)

--- src/test/java/school/redrover/AnaBelGroupTest.java
+++ src/test/java/school/redrover/AnaBelGroupTest.java
@@ -14,4 +14,21 @@ public class AnaBelGroupTest extends BaseTest {
         Assert.assertTrue(logo.isDisplayed());
     }
 
+    @Test
+    public void testAddDescription() {
+        WebElement button = getDriver().findElement(By.xpath("//a[@id='description-link']"));
+        button.click();
+
+        WebElement textField = getDriver().findElement(By.cssSelector(".jenkins-input"));
+        textField.clear();
+        textField.sendKeys("testDesctiprion1");
+
+        WebElement buttonSave = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        buttonSave.click();
+
+        WebElement verify = getDriver().findElement(By.cssSelector("#description>div"));
+
+        Assert.assertEquals(verify.getText(), "testDesctiprion1");
+    }
+
 }
