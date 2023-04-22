d4652cfaad4fe265e881be3372590ac9fb2910b8
Vitalii Dim <127656806+Vidim83@users.noreply.github.com>
2023-04-22 15:57:50 +0300

    First jenkins test (#912)
    * add first first_jenkins_test
    
    * add create item test
    
    * add create_item_test
    
    ---------
    
    Co-authored-by: Vidim83 <08091985n@gmail.com>

--- src/test/java/school/redrover/JasperGroupTest.java
+++ src/test/java/school/redrover/JasperGroupTest.java
@@ -23,4 +23,33 @@ public class JasperGroupTest extends BaseTest {
         WebElement actualResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"main-panel\"]/div[2]")));
         Assert.assertEquals(actualResult.getText(), "Jenkins User ID: admin");
     }
+
+    @Test
+    public void createNewItem() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        WebElement createItem = getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a"));
+        createItem.click();
+
+        WebElement nameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id=\"name\"]")));
+        nameField.sendKeys("New Item");
+
+        WebElement typeOfItems = getDriver().findElement(By.xpath("//*[@class=\"hudson_model_FreeStyleProject\"]"));
+        typeOfItems.click();
+
+        WebElement okButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"ok-button\"]")));
+        okButton.click();
+
+        WebElement descriptionField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name=\"description\"]")));
+        descriptionField.sendKeys("New Item");
+
+        WebElement applyButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class=\"jenkins-button apply-button\"]")));
+        applyButton.click();
+
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class=\"jenkins-button jenkins-button--primary \"]")));
+        saveButton.click();
+
+        WebElement actualResult = getDriver().findElement(By.xpath("//*[@class=\"job-index-headline page-headline\"]"));
+        Assert.assertEquals(actualResult.getText(),"Project New Item");
+    }
+
 }
