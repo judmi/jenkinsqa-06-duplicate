de78ff3d126b3e58f2b93dedcc740dbc847ddd71
Ivan Korolev <ikorolev13@gmail.com>
2023-04-23 11:33:52 +0400

    GroupLargusTest/IvanK/ add first test at Jenkins (#916)
    * GroupLargusTest/IvanK/ add first test at Jenkins
    
    * GroupLargusTest/IvanK/ add first test at Jenkins with change
    
    * GroupLargusTest/IvanK/ add second test
    
    * GroupLargusTest/IvanK/ add second test with correct

--- src/test/java/school/redrover/CatGroupTest.java
+++ src/test/java/school/redrover/CatGroupTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
