5fb4da4f766552e37801309714a64ea5f0134d9c
verafes <verafes@gmail.com>
2023-04-22 23:14:10 -0700

    testNewItemHeader() added

---
+++ src/test/java/school/redrover/NewItemTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+
+public class NewItemTest extends BaseTest {
+
+    @Test
+    public void testNewItemHeader() {
+        getDriver().findElement(By.linkText("New Item")).click();
+
+        WebElement h3Header = new WebDriverWait(getDriver(), Duration.ofMillis(3000))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@class = 'h3']")));
+        String actualResult = h3Header.getText();
+
+        Assert.assertEquals(actualResult, "Enter an item name");
+    }
+}
