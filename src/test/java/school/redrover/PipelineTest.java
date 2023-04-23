5ca2df5b98cba72dea3eb3451d7a7a3582a46f6a
Irina Irico <85979179+Iriskus@users.noreply.github.com>
2023-04-23 01:05:22 -0500

    Revert "TestCreatePipelineWithoutParameters" (#943)

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -1,7 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,21 +11,43 @@ import school.redrover.runner.BaseTest;
 import java.time.Duration;
 
 public class PipelineTest extends BaseTest {
-    private static final String PIPELINE_NAME = "pipeline1";
 
+    public WebDriverWait webDriverWait10;
+
+    public void scrollByElement(By by) throws InterruptedException {
+        WebElement scroll =getDriver().findElement(by);
+        new Actions(getDriver())
+                .scrollToElement(scroll)
+                .perform();
+    }
+
+    public final WebDriverWait getWait10() {
+        if (webDriverWait10 == null) {
+            webDriverWait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+        return webDriverWait10;
+    }
     @Test
-    public void testCreatePipelineWithoutParameters() throws InterruptedException {
-        getDriver().findElement(By.linkText("New Item")).click();
-        new WebDriverWait(getDriver(), Duration.ofMillis(1500)).until(ExpectedConditions.elementToBeClickable(By.id("name"))).sendKeys(PIPELINE_NAME);
-        getDriver().findElement(By.xpath("//span[text() = 'Pipeline']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        new WebDriverWait(getDriver(), Duration.ofMillis(2000)).until(ExpectedConditions.elementToBeClickable(By.
-                xpath("//button[contains(@class,'jenkins-button jenkins-button--primary')]"))).click();
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
+    public void testCreatePipeline() throws InterruptedException {
 
-        String actualResult = getDriver().findElement(By.xpath("//tr[@id = 'job_" + PIPELINE_NAME + "']//a[@href='job/"+PIPELINE_NAME+"/']")).getText();
+        WebElement jobName = getDriver().findElement(By.cssSelector(".content-block__link>span"));
+        jobName.click();
 
-        Assert.assertEquals(actualResult, PIPELINE_NAME);
+        WebElement field = getDriver().findElement(By.cssSelector("#name"));
+        field.sendKeys("Pipe");
 
+        WebElement pipeline = getDriver().findElement(By.xpath("//span[text()='Pipeline']"));
+        pipeline.click();
+
+        WebElement buttonOk = getDriver().findElement(By.cssSelector("#ok-button"));
+        buttonOk.click();
+
+        scrollByElement(By.cssSelector(".page-footer__links.page-footer__links--white.jenkins_ver>a"));
+        WebElement save = getDriver().findElement(By.cssSelector(".jenkins-button.jenkins-button--primary"));
+        save.click();
+
+        WebElement pipelinePipe= getDriver().findElement(By.cssSelector("#main-panel>h1"));
+
+        Assert.assertEquals(pipelinePipe.getText(), "Pipeline Pipe");
     }
 }
