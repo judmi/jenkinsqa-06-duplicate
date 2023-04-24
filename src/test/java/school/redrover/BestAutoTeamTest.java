601e3e63817a14c3f72575642e92cf36c2cb7b49
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2023-04-24 10:35:05 +0300

    refactor code (#968)

--- src/test/java/school/redrover/BestAutoTeamTest.java
+++ src/test/java/school/redrover/BestAutoTeamTest.java
@@ -9,52 +9,44 @@ import school.redrover.runner.BaseTest;
 public class BestAutoTeamTest extends BaseTest {
 
     @Test
-    public void isJenkinsLoad() throws InterruptedException {
+    public void testIsJenkinsLoad() throws InterruptedException {
 
-        WebElement jenkinsLogo = getDriver().findElement(
-                By.xpath("//*[@id='jenkins-home-link']"));
+        WebElement jenkinsLogo = getDriver().findElement(By.xpath("//*[@id='jenkins-home-link']"));
 
         Assert.assertTrue(jenkinsLogo.isDisplayed());
     }
 
     @Test
-    public void addDescriptionTest() throws InterruptedException {
+    public void testAddDescription() throws InterruptedException {
 
         getDriver().findElement(By.xpath("//*[@id='description-link']")).click();
 
         getDriver().findElement(By.xpath("//textarea[@name='description']")).clear();
 
-        getDriver().findElement(By.xpath("//textarea[@name='description']"))
-                .sendKeys("TestDescription");
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("TestDescription");
 
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//div[@id='description']/*[1]")).getText()
-                , "TestDescription");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/*[1]")).getText(), "TestDescription");
     }
-    
+
     @Test
-    public void editDescriptionTest() throws InterruptedException {
+    public void testEditDescription() throws InterruptedException {
 
         getDriver().findElement(By.xpath("//*[@id='description-link']")).click();
 
         getDriver().findElement(By.xpath("//textarea[@name='description']")).clear();
 
-        getDriver().findElement(By.xpath("//textarea[@name='description']"))
-                .sendKeys("TestDescription");
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("TestDescription");
 
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
         getDriver().findElement(By.xpath("//*[@id='description-link']")).click();
 
-        getDriver().findElement(By.xpath("//textarea[@name='description']"))
-                .sendKeys("Edited");
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("Edited");
 
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//div[@id='description']/*[1]")).getText()
-                , "EditedTestDescription");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/*[1]")).getText(), "EditedTestDescription");
     }
 }
