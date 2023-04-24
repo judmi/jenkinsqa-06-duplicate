cb889ef64f5d479b9d3e46ffb2bed3c4645d0b35
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2023-04-24 12:36:18 -0500

    testViewIconButtonsPeoplePage() (#1005)
    * Create testViewIconButtonsPeoplePage(), create method ListText(List<WebElement> elementList)

--- src/test/java/school/redrover/NeedMoreCoffeeTest.java
+++ src/test/java/school/redrover/NeedMoreCoffeeTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -13,6 +14,7 @@ import java.time.Duration;
 
 public class NeedMoreCoffeeTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testIdUser() {
         WebElement buttonPeople = getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[2]/span/a"));
--- src/test/java/school/redrover/PeoplePageTest.java
+++ src/test/java/school/redrover/PeoplePageTest.java
@@ -6,7 +6,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PeoplePageTest extends BaseTest {
+
+    public static List<String> ListText(List<WebElement> elementList) {
+        List<String> stringList = new ArrayList<>();
+        for (WebElement element : elementList) {
+            stringList.add(element.getText());
+        }
+        return stringList;
+    }
 
     @Test
     public void testViewPeoplePage() {
@@ -15,5 +26,17 @@ public class PeoplePageTest extends BaseTest {
         WebElement nameOfPeoplePageHeader = getDriver().findElement(By.xpath("//h1"));
 
         Assert.assertEquals(nameOfPeoplePageHeader.getText(), "People");
+    }
+
+    @Test
+    public void testViewIconButtonsPeoplePage() {
+        List expectedIconButtonsNames = List.of("S\n" + "mall", "M\n" + "edium", "L\n" + "arge");
+
+        WebElement peopleSideMenu = getDriver().findElement(By.xpath("//span/a[@href='/asynchPeople/']"));
+        peopleSideMenu.click();
+        List<WebElement> iconButtons = getDriver().findElements(By.xpath("//div[@class='jenkins-icon-size']//ol/li"));
+        List<String> actualIconButtonsNames = ListText(iconButtons);
+
+        Assert.assertEquals(actualIconButtonsNames, expectedIconButtonsNames);
     }
 }
