e64dccaeaedcb1a3b6905ff4ee1214e4288ce920
AnastasiyaAbramova <128148850+AnastasiyaAbramova@users.noreply.github.com>
2023-04-24 17:36:41 +0500

    if the user presses CTRL+K cursor is active in search box (#960)

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -1,7 +1,25 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
+import java.time.Duration;
 public class CaramelSyrupForJavaTest extends BaseTest {
+    @Test
+    public void testAbramovaHotKeys() {
+
+        WebElement body = getDriver().findElement(By.tagName("body"));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        body.sendKeys(Keys.chord(Keys.CONTROL, "k"));
+        WebElement searchBox = getDriver().findElement(By.xpath("//input[@role]"));
+        WebElement currentElement = getDriver().switchTo().activeElement();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        Assert.assertEquals(searchBox, currentElement);
+    }
 
 }
+
+
