056094b12802dba53e4ba0cd31bd95536ad8a626
Artem Savichev <90255887+SavaSavichev@users.noreply.github.com>
2023-04-24 15:44:35 +0600

    add first savichev jenkins test (#985)

--- src/test/java/school/redrover/GroupJavaciraptorsTest.java
+++ src/test/java/school/redrover/GroupJavaciraptorsTest.java
@@ -13,4 +13,13 @@ public class GroupJavaciraptorsTest extends BaseTest {
 
         Assert.assertEquals(welcomeElement.getText(),"Welcome to Jenkins!");
     }
+
+    @Test
+    public void savichevTest() {
+        WebElement versionElement = getDriver().findElement(By.xpath("//a[normalize-space()='Jenkins 2.387.2']"));
+        Assert.assertEquals(versionElement.getText(), "Jenkins 2.387.2");
+
+        WebElement textElement = getDriver().findElement(By.xpath("//h2[normalize-space()='Set up a distributed build']"));
+        Assert.assertEquals(textElement.getText(), "Set up a distributed build");
+    }
 }
