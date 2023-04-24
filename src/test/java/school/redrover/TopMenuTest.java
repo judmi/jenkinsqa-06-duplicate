098abac7cf1cdbbdbfae51792d945b92e564c95f
elenavanesova888 <elenavanesova888@gmail.com>
2023-04-23 21:36:20 -0500

    Add test User button and User dropdown

--- src/test/java/school/redrover/TopMenuTest.java
+++ src/test/java/school/redrover/TopMenuTest.java
@@ -2,11 +2,26 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TopMenuTest extends BaseTest {
+
+    public List<String> getNamesOfLists(List<WebElement> elements) {
+        List<String> texts = new ArrayList<>();
+
+        for (WebElement element : elements) {
+            texts.add(element.getText());
+        }
+
+        return texts;
+    }
 
     @Test
     public void testCheckPeopleButton() {
@@ -14,5 +29,27 @@ public class TopMenuTest extends BaseTest {
         boolean actualResult = buttonPeople.isDisplayed();
 
         Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void testTopMenuUser(){
+        WebElement topMenuUser = getDriver().findElement(By.xpath("//span[@class='hidden-xs hidden-sm'][text()='admin']"));
+
+        String actualResult1 = topMenuUser.getText();
+
+        Assert.assertEquals(actualResult1, "admin");
+    }
+
+    @Test
+    public void testTopMenuUserDropDown() {
+        Actions actions = new Actions(getDriver());
+        WebElement topMenuUserDropDown = getDriver().findElement(By.xpath("//div/a[@class='model-link']/button[@class='jenkins-menu-dropdown-chevron']"));
+        actions.click(topMenuUserDropDown).perform();
+
+        List<WebElement> listTopElements = getDriver().findElements(By.xpath("//ul[@class='first-of-type']/li"));
+        List<String> expected = Arrays.asList("Builds", "Configure", "My Views", "Credentials");
+        List<String>  actual = getNamesOfLists(listTopElements);
+
+        Assert.assertEquals(actual,expected);
     }
 }
