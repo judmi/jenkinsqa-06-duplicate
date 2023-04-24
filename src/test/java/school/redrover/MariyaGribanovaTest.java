21344f6ed0bb5c672b2738ed721bb728f21c5cb3
Mariya Gribanova <gribanova.masher@gmail.com>
2023-04-24 23:23:24 +0300

    Add second test

--- src/test/java/school/redrover/MariyaGribanovaTest.java
+++ src/test/java/school/redrover/MariyaGribanovaTest.java
@@ -1,27 +1,55 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
-
+import static org.testng.Assert.assertEquals;
 
 public class MariyaGribanovaTest {
 
     @Test
-    public void testMGFirst() {
+    public void testFirstMG() throws InterruptedException {
 
-        //System.setProperty("webdriver.chrome.driver","/path/to/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://www.google.com/");
+
+        WebElement textBox = driver.findElement(By.name("q"));
+
+        textBox.sendKeys("selenium");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+
+        Assert.assertEquals(text.getText(), "Selenium");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSecondMG() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
         String title = driver.getTitle();
         Assert.assertEquals("Web form", title);
+
+        Thread.sleep(2000);
 
         WebElement textBox = driver.findElement(By.name("my-text"));
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
@@ -35,4 +63,5 @@ public class MariyaGribanovaTest {
 
         driver.quit();
     }
+
 }
