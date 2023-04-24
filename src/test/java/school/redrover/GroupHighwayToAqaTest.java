ab21b0aedf838fb076093436debe068179fe7274
Ruslan Fursa <102952979+ruslanfursa@users.noreply.github.com>
2023-04-24 16:08:14 +0300

    add implicitlyWait to testAddBoardDescription (#987)
    * add implicityWait to testAddBoardDescription
    
    * add removing of previous tests
    
    * refactor

--- src/test/java/school/redrover/GroupHighwayToAqaTest.java
+++ src/test/java/school/redrover/GroupHighwayToAqaTest.java
@@ -3,33 +3,41 @@ package school.redrover;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 
+
 public class GroupHighwayToAqaTest extends BaseTest {
 
-    @Ignore
     @Test
     public void testAddBoardDescription() {
         String description = "Some text about dashboard";
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
         WebElement addDescriptionButton = getDriver().findElement(By.xpath("//div/a [@id = 'description-link']"));
         addDescriptionButton.click();
+
         WebElement inputForm = getDriver().findElement(By.xpath("//div[@class = 'setting-main help-sibling']/textarea"));
+        inputForm.clear();
         inputForm.sendKeys(description);
+
         WebElement saveButton = getDriver().findElement(By.xpath("//div/button[@name = 'Submit']"));
         saveButton.click();
+
         WebElement descriptionText = getDriver().findElement(By.xpath("//*[@id='description']/div[1]"));
         Assert.assertEquals(descriptionText.getText(), description);
+
         addDescriptionButton = getDriver().findElement(By.xpath("//div/a [@id = 'description-link']"));
         addDescriptionButton.click();
         getDriver().findElement(By.xpath("//div[@class = 'setting-main help-sibling']/textarea")).clear();
+
         saveButton = getDriver().findElement(By.xpath("//div/button[@name = 'Submit']"));
         saveButton.click();
     }
