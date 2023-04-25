09b5ea718802254394b12f31cff21cd9ab352632
ArtyomDulya <111312308+ArtyomDulya@users.noreply.github.com>
2023-04-25 10:40:44 +0500

    Newprojectcheatetest (#1039)
    * replaceSleepToWait
    
    * delete code lines provided in BaseTest (#575)
    
    * GroupForwardTest->Refactor sauceDemoLoginSuccessTest (#578)
    
    * add testJava (#563)
    
    * Syntactic changes are made
    
    * debug (#579)
    
    * new_project_create_test
    
    * learn_more_test
    
    ---------
    
    Co-authored-by: okila <37234241+okila-ok@users.noreply.github.com>
    Co-authored-by: MarinaAvr <118004084+MarinaAvr@users.noreply.github.com>
    Co-authored-by: Svetlana Zhits <113052741+szhits@users.noreply.github.com>
    Co-authored-by: Marina Loseva <marynalos@yandex.ru>

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,6 +10,7 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import java.time.Duration;
+import java.util.ArrayList;
 
 public class CaramelSyrupForJavaTest extends BaseTest {
 
@@ -87,6 +89,56 @@ public class CaramelSyrupForJavaTest extends BaseTest {
 
         Assert.assertEquals(actResFol, expResFol);
         Assert.assertEquals(actResName, expResName);
+    }
+
+    @Test
+    public void testADCreateJobProject() {
+        String expectedResultSummary = "Project Engineer";
+        String expectedResultDescription = "New Project";
+
+        WebElement createJob = getDriver().findElement(By.xpath("//span[text()='Create a job']"));
+        createJob.click();
+        WebElement inputLine = getDriver().findElement(By.xpath("//input[@id='name']"));
+        inputLine.click();
+        inputLine.sendKeys("Engineer");
+        WebElement freestyleProject = getDriver().findElement(By.xpath("//span[text()='Freestyle project']"));
+        freestyleProject.click();
+        WebElement buttonOk = getDriver().findElement(By.xpath("//button[@id='ok-button']"));
+        buttonOk.click();
+
+        WebElement description = getDriver().findElement(By.xpath("//textarea[@name='description']"));
+        description.click();
+        description.sendKeys("New Project");
+        WebElement buttonSubmit = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        buttonSubmit.click();
+
+        WebElement newSummaryProject = getDriver().findElement(By.xpath("//h1[text()='Project Engineer']"));
+        String actualResultSummary = newSummaryProject.getText();
+        WebElement newDescription = getDriver().findElement(By.xpath("//div[@id='description']//div[text()='New Project']"));
+        String actualResultDescription = newDescription.getText();
+
+        Assert.assertEquals(actualResultSummary, expectedResultSummary);
+        Assert.assertEquals(actualResultDescription, expectedResultDescription);
+    }
+
+    @Test
+    public void testADLearnMore() {
+        String expectedResult = "static content of the Wiki";
+        WebElement learnMoreHref = getDriver().findElement(By.xpath("//a[@href='https://www.jenkins.io/redirect/distributed-builds']"));
+        learnMoreHref.click();
+        ArrayList<String> windows = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(windows.get(1));
+
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        WebElement atlassianHref = getDriver().findElement(By.xpath("//a[@href='/display/']"));
+        atlassianHref.click();
+        WebElement wikiJenkinsHref = getDriver().findElement(By.xpath("//a[text()='static content of the Wiki']"));
+        String actualResult = wikiJenkinsHref.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
     }
 }
 
