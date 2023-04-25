dc98eec29c60924d10b3470ae40f9ef3136f2c13
BoykoArtem <130305693+BoykoArtem@users.noreply.github.com>
2023-04-25 13:19:20 +0300

    Header test (#966)
    * add brand-new slider test
    
    * fixed some issues
    
    * ignored unstable test
    
    * Add basic header tests
    
    * Conflict resolve
    
    * Conflict resolve
    
    * Conflict resolve

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -1,12 +1,49 @@
 package school.redrover;
-
-import jdk.jfr.Description;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import static org.testng.Assert.assertEquals;
+import jdk.jfr.Description;
 
 public class HeaderTest extends BaseTest {
+    @Test
+    public void testHeaderLogoIcon() {
+        WebElement logoIcon = getDriver().findElement(By.xpath("//*[@id=\"jenkins-head-icon\"]"));
+        Assert.assertTrue(logoIcon.isDisplayed());
+    }
+
+    @Test
+    public void testHeaderLogoText() {
+        WebElement logoText = getDriver().findElement(By.xpath("//*[@id=\"jenkins-name-icon\"]"));
+        Assert.assertTrue(logoText.isDisplayed());
+    }
+
+    @Test
+    public void testSearchTextField() throws InterruptedException {
+        Actions hover = new Actions(getDriver());
+
+        WebElement searchTextBox = getDriver().findElement(By.xpath("//*[@id=\"search-box\"]"));
+        WebElement searchIcon = getDriver().findElement(By.cssSelector(".main-search__icon-leading svg"));
+        WebElement helpButton = getDriver().findElement(By.xpath("//*[@id=\"searchform\"]/a"));
+        WebElement helpButtonIcon = getDriver().findElement(By.cssSelector(".main-search__icon-trailing svg"));
+
+        String placeholder = searchTextBox.getAttribute("placeholder");
+        String defaultHelpButtonColor = helpButton.getCssValue("color");
+
+        Assert.assertTrue(searchIcon.isDisplayed());
+        assertEquals(placeholder, "Search (CTRL+K)");
+        Assert.assertTrue(helpButtonIcon.isDisplayed());
+        assertEquals(defaultHelpButtonColor,"rgba(115, 115, 140, 1)");
+
+        hover.moveToElement(helpButton).perform();
+        Thread.sleep(500);
+        String hoverHelpButtonColor = helpButton.getCssValue("color");
+
+        assertEquals(hoverHelpButtonColor, "rgba(64, 64, 64, 1)");
+    }
 
     @Description("Verify the placeholder text in the search field")
     @Test
