a40ec97ce730d7c25d44d1aa904459c70f23df93
DMITRII DENISOV <122356423+dmitriidn@users.noreply.github.com>
2023-04-25 13:01:13 +0300

    Comrades/dm/create jenkins job test (#1052)
    Create jenkins job

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import java.time.Duration;
@@ -60,7 +61,7 @@ public class CaramelSyrupForJavaTest extends BaseTest {
         Assert.assertTrue(error.isDisplayed());
         Assert.assertFalse(notError.isDisplayed());
     }
-
+    @Ignore
     @Test
     public void testDimaKFirst() {
         String expResFol = "Folder";
