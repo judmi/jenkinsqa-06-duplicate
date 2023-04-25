f249ffe4dcf2a21821d170510871ee08b36f538a
ReginaKurtLV <128086386+ReginaKurtLV@users.noreply.github.com>
2023-04-24 21:51:42 -0700

    Jenkins test (#1037)
    Co-authored-by: ReginaKurtLV <rgaynul@gmail.com>

--- src/test/java/school/redrover/JavaJitsuGroupTest.java
+++ src/test/java/school/redrover/JavaJitsuGroupTest.java
@@ -30,7 +30,6 @@ public class JavaJitsuGroupTest extends BaseTest {
     public void logoCheckingTest() {
         WebElement logoCheck = getDriver().findElement(By.xpath("//img[@id='jenkins-head-icon']"));
         Assert.assertTrue(logoCheck.isDisplayed());
-
     }
 
     @Test
@@ -81,8 +80,6 @@ public class JavaJitsuGroupTest extends BaseTest {
         WebElement myViews = getDriver().findElement(By.xpath("//div[5]//span[1]//a[1]"));
         Thread.sleep(2000);
         Assert.assertEquals(myViews.getText(), "My Views");
-
-
     }
 
     @Test
@@ -96,5 +93,54 @@ public class JavaJitsuGroupTest extends BaseTest {
 
         WebElement credentialsPage = getDriver().findElement(By.xpath("//div[@id='main-panel']/div/div/h1"));
         Assert.assertEquals(credentialsPage.getText(), "Credentials");
+    }
+
+    @Test
+    public void testNewItemFolder() {
+
+        WebElement newItemElement = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
+        newItemElement.click();
+        WebElement name = getDriver().findElement(By.id("name"));
+        name.sendKeys("newProject");
+        WebElement folder = getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']"));
+        folder.click();
+        WebElement okButton = getDriver().findElement(By.cssSelector("#ok-button"));
+        okButton.click();
+        Assert.assertEquals("General", getDriver().findElement(By.cssSelector("#general")).getText());
+
+        WebElement displayName = getDriver().findElement(By.name("_.displayNameOrNull"));
+        displayName.sendKeys("NewTest");
+        WebElement description = getDriver().findElement(By.name("_.description"));
+        description.sendKeys("Test project to QA Redrover.school");
+
+        WebElement submit = getDriver().findElement(By.name("Submit"));
+        submit.click();
+        Assert.assertEquals("NewTest", getDriver().findElement(By.cssSelector("div[id='main-panel'] h1")).getText());
+    }
+
+    @Test
+    public void testNewUser() {
+
+        WebElement manageJenkins = getDriver().findElement(By.cssSelector("a[href='/manage']"));
+        manageJenkins.click();
+        WebElement manageUsers = getDriver().findElement(By.cssSelector("a[href='securityRealm/']"));
+        manageUsers.click();
+        WebElement createUserButton = getDriver().findElement(By.xpath(" //a[@class='jenkins-button jenkins-button--primary']"));
+        createUserButton.click();
+
+        WebElement username = getDriver().findElement(By.name("username"));
+        username.sendKeys("NewUser");
+        WebElement password = getDriver().findElement(By.name("password1"));
+        password.sendKeys("NewUser12345!");
+        WebElement confirmPassword = getDriver().findElement(By.name("password2"));
+        confirmPassword.sendKeys("NewUser12345!");
+        WebElement fullName = getDriver().findElement(By.name("fullname"));
+        fullName.sendKeys("Max Smith");
+        WebElement email = getDriver().findElement(By.name("email"));
+        email.sendKeys("max@gmail.com");
+
+        WebElement submit = getDriver().findElement(By.name("Submit"));
+        submit.click();
+        Assert.assertEquals("NewUser", getDriver().findElement(By.xpath("//tbody/tr[2]/td[2]")).getText());
     }
 }
