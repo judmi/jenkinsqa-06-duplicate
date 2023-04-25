cf883c77e18cd7dc76106b0aa0ec2d81e61cfebe
ainuramergalimova <mergalimovaainur@gmail.com>
2023-04-25 13:38:50 -0700

    fix Test

--- src/test/java/school/redrover/AnaBelGroupTest.java
+++ src/test/java/school/redrover/AnaBelGroupTest.java
@@ -1,58 +1,35 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import school.redrover.runner.BaseTest;
-
-public class AnaBelGroupTest extends BaseTest {
-
-    @Test
-    public void testLogo() {
-        WebElement logo = getDriver().findElement(By.id("jenkins-name-icon"));
-        Assert.assertTrue(logo.isDisplayed());
-    }
-
-    @Test
-    public void testBuildHistory() {
-        WebElement buildHistory = getDriver().findElement(By.xpath("//a[@href ='/view/all/builds']"));
-
-        Assert.assertEquals(buildHistory.getText(), "Build History");
-    }
-
-    public void testAddDescription() {
-        WebElement button = getDriver().findElement(By.xpath("//a[@id='description-link']"));
-        button.click();
-
-        WebElement textField = getDriver().findElement(By.cssSelector(".jenkins-input"));
-        textField.clear();
-        textField.sendKeys("testDesctiprion1");
-
-        WebElement buttonSave = getDriver().findElement(By.xpath("//button[@name='Submit']"));
-        buttonSave.click();
-
-        WebElement verify = getDriver().findElement(By.cssSelector("#description>div"));
-
-        Assert.assertEquals(verify.getText(), "testDesctiprion1");
-    }
-}
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-
-
-import static org.testng.Assert.assertEquals;
+import school.redrover.runner.BaseTest;
 
 public class AnaBelGroupTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    private void beforeMethod() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        driver = new ChromeDriver(chromeOptions);
+    }
+
+    @AfterMethod
+    private void afterMethod() {
+        driver.quit();
+    }
 
     @Test
-    public void testAinura () throws InterruptedException {
-        //ChromeOptions chromeOptions = new ChromeOptions();
-       // chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+    public void testSeleniumTitle () throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
@@ -67,13 +44,7 @@ public class AnaBelGroupTest {
 
         textBox.sendKeys("Selenium");
         submitButton.click();
+    }}
 
-        //WebElement message;
-      //  message = driver.findElement(By.id("message)"));
-       // String value = message.getText();
-       // Assert.assertEquals("Recived", value);
 
-        driver.quit();
-    }
-    }
 
