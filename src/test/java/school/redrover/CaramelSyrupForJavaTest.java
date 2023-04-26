f4110392a1d04c4ef75bbadb6cb3097967442752
KathyGU <97336394+KathyGU@users.noreply.github.com>
2023-04-26 17:46:08 +0300

    my first Jenkins test (#1123)
    * my first Jenkins test
    
    * testAbramovaDropDownList_Ignored

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -142,7 +142,7 @@ public class CaramelSyrupForJavaTest extends BaseTest {
         Assert.assertEquals(actualResult, expectedResult);
 
     }
-
+    @Ignore
     @Test
     public  void testAbramovaDropDownList() {
         List<String> expectedResult = Arrays.asList("Builds", "Configure", "My Views", "Credentials");
