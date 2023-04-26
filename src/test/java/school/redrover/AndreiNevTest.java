e39be5f2b13c4411fd88466acb991b4ef8937251
andrei-neviadomski <126451904+andrei-neviadomski@users.noreply.github.com>
2023-04-26 17:49:09 +0200

    move my test from the wrong directory (#1130)

--- src/test/java/school/redrover/AndreiNevTest.java
+++ src/test/java/school/redrover/AndreiNevTest.java
@@ -1,11 +1,12 @@
-package school.redrover.runner;
+package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
-public class AndreiNevTest extends BaseTest{
+public class AndreiNevTest extends BaseTest {
 
     @Test
     public void testFirstJenkins() {
