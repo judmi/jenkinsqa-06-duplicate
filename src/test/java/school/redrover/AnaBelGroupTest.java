0b5ecb967dbe14b9439cd4703090ed8ac30dd15a
TatyanaAst <130398289+TatyanaAst@users.noreply.github.com>
2023-04-27 01:51:02 +0300

    corrected Item test (#1114)
     Add Item test

--- src/test/java/school/redrover/AnaBelGroupTest.java
+++ src/test/java/school/redrover/AnaBelGroupTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -36,4 +37,27 @@ public class AnaBelGroupTest extends BaseTest {
 
         Assert.assertEquals(verify.getText(), "testDesctiprion1");
     }
+
+    @Test
+    public void testItem() {
+
+        WebElement button = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
+        button.click();
+
+        WebElement textBox = getDriver().findElement(By.xpath("//input[@name='name']"));
+        textBox.sendKeys("Item");
+
+        WebElement lableButton = getDriver().findElement(By.xpath("//span[contains(text(),'Multi-configuration project')]"));
+        lableButton.click();
+
+        WebElement okButton = getDriver().findElement(By.xpath("//*[@id='ok-button']"));
+        okButton.click();
+
+        WebElement validateButton = getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate'][@name='Submit']"));
+        validateButton.click();
+
+        WebElement general = getDriver().findElement(By.xpath("//h1[@class='matrix-project-headline page-headline']"));
+        Assert.assertEquals(general.getText(), "Project Item");
+    }
 }
+
