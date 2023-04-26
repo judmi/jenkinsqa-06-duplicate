371fd0be1ba8cb97fd4209943f1ec3b7ccb87f33
Anna Morozova <105454023+morozanna503@users.noreply.github.com>
2023-04-26 04:30:58 +0400

    added new class MultiConfiguration (#1095)
    * added new class MultiConfiguration
    
    * added new class MultiConfiguration

---
+++ src/test/java/school/redrover/MultiConfigurationTest.java
@@ -0,0 +1,24 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class MultiConfigurationTest extends BaseTest {
+
+    @Test
+    public void testCreateMultiConfig() {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("test");
+        getDriver().findElement(By.xpath("//span[text()='Multi-configuration project']")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("test");
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//div[@id='description']")).isDisplayed());
+    }
+
+}
