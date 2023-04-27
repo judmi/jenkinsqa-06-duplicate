d9acab24990559a67bc0c9d36163738ad8b4cc1a
AlexandraNovikA26 <ya.sasha-novik@yandex.by>
2023-04-26 21:38:04 -0500

    simplified test

--- src/test/java/school/redrover/AlexTest.java
+++ src/test/java/school/redrover/AlexTest.java
@@ -1,9 +1,6 @@
 package school.redrover;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -13,15 +10,8 @@ public class AlexTest extends BaseTest {
     @Test
     public void testAddMultiConfProject() {
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        Actions action = new Actions(getDriver());
-        JavascriptExecutor js = (JavascriptExecutor)getDriver();
-
-        WebElement dashBoard = getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']"));
-        action.moveToElement(dashBoard).perform();
-        WebElement dashBoardDropDown = getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']/button"));
-        action.click(dashBoardDropDown).perform();
-
-        js.executeScript("arguments[0].click();", getDriver().findElement(By.xpath("//div[@id='breadcrumb-menu-target']//a[@href='/view/all/newJob']")));
+        WebElement addNewItem = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
+        addNewItem.click();
 
         WebElement itemNameField = getDriver().findElement(By.xpath("//input[@id='name']"));
         itemNameField.sendKeys("Sample project");
