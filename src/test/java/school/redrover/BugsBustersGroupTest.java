e33b8e06a8036398f8df50d20c1064e815d3fe3a
Angelina Did <65321154+GelikaUS@users.noreply.github.com>
2023-04-27 02:40:23 -0400

    BugsBusters/AD (#1166)
    * Create group class, add 2 tests
    
    * Added 4 tests

--- src/test/java/school/redrover/BugsBustersGroupTest.java
+++ src/test/java/school/redrover/BugsBustersGroupTest.java
@@ -7,19 +7,22 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 public class BugsBustersGroupTest extends BaseTest {
 
     @Test
-    public void testAngelinaHeadIconIsDisplayed() {
+    public void testHeadIconIsDisplayed() {
         WebElement icon = getDriver().findElement(By.id("jenkins-head-icon"));
 
         Assert.assertTrue(icon.isDisplayed());
     }
 
     @Test
-    public void testAngelinaDashboardButtonIsDisplayed() {
-        WebElement dashboardButton = getDriver().findElement(By.xpath("//button[@class='jenkins-menu-dropdown-chevron']"));
+    public void testDashboardButtonIsDisplayed() {
+        WebElement dashboardButton = getDriver().findElement(By.xpath("//*[@id='breadcrumbs']//button"));
 
         Assert.assertTrue(dashboardButton.isDisplayed());
     }
@@ -62,5 +65,77 @@ public class BugsBustersGroupTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//*[@class = 'textarea-show-preview']"))
                 .getText(), "Preview");
+    }
+
+    @Test
+    public void testOkButtonIsDisabled() {
+        WebElement newItem = getDriver().findElement(By.xpath("//*[@id='tasks']//a"));
+        newItem.click();
+
+        WebElement okButton = getDriver().findElement(By.id("ok-button"));
+
+        Assert.assertFalse(okButton.isEnabled(), "Button is disabled");
+    }
+
+    @Test
+    public void testOkButtonIsEnabled() {
+        WebElement newItem = getDriver().findElement(By.xpath("//*[@id='tasks']//a"));
+        newItem.click();
+
+        WebElement name = getDriver().findElement(By.id("name"));
+        name.sendKeys("Test-Folder");
+
+        WebElement newFolder = getDriver().findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']//li[1]"));
+        newFolder.click();
+
+        WebElement okButton = getDriver().findElement(By.id("ok-button"));
+
+        Assert.assertTrue(okButton.isEnabled(), "Button is enabled");
+    }
+
+    @Test
+    public void testNewFolderIsCreated() {
+        WebElement newItem = getDriver().findElement(By.xpath("//*[@id='tasks']//a"));
+        newItem.click();
+
+        WebElement name = getDriver().findElement(By.id("name"));
+        name.sendKeys("Test-Folder");
+
+        WebElement newFolder = getDriver().findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']//li[1]"));
+        newFolder.click();
+
+        WebElement okButton = getDriver().findElement(By.id("ok-button"));
+        okButton.click();
+
+        WebElement dashboardButton = getDriver().findElement(By.xpath("//*[@id='breadcrumbs']//a"));
+        dashboardButton.click();
+
+        WebElement createdFolder = getDriver().findElement(By.id("job_Test-Folder"));
+
+        Assert.assertTrue(createdFolder.isDisplayed(), "Created folder is displayed");
+    }
+
+    @Test
+    public void testMultibranchPipeline() {
+        WebElement newItem = getDriver().findElement(By.xpath("//*[@id='tasks']//a"));
+        newItem.click();
+
+        WebElement name = getDriver().findElement(By.id("name"));
+        name.sendKeys("Test-Multibranch-Pipeline");
+
+        WebElement newPipeline = getDriver().findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']//li[2]"));
+        newPipeline.click();
+
+        WebElement okButton = getDriver().findElement(By.id("ok-button"));
+        okButton.click();
+
+        List<WebElement> configurationMenuItems = getDriver().findElements(By.xpath("//*[@id='tasks']//button"));
+        List<String> expectedMenuItems = Arrays.asList("General", "Branch Sources", "Build Configuration",
+                "Scan Multibranch Pipeline Triggers", "Orphaned Item Strategy", "Appearance", "Health metrics", "Properties");
+
+        for (int i = 0; i < configurationMenuItems.size(); i++) {
+            String actualMenuItems = configurationMenuItems.get(i).getText();
+            Assert.assertEquals(actualMenuItems, expectedMenuItems.get(i));
+        }
     }
 }
