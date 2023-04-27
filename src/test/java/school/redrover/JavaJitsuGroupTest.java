172d14cd3ce72f8afc14a2daf47d5be91a74e4dd
Dasha Turevich <dasha.turevich@yandex.ru>
2023-04-27 21:48:59 +0300

    add test for Jenkins3

--- src/test/java/school/redrover/JavaJitsuGroupTest.java
+++ src/test/java/school/redrover/JavaJitsuGroupTest.java
@@ -207,4 +207,16 @@ public class JavaJitsuGroupTest extends BaseTest {
         WebElement manageJenkinsHeader = getDriver().findElement(By.xpath("//h1"));
         Assert.assertEquals(manageJenkinsHeader.getText(), "Manage Jenkins");
     }
+    @Test
+    public void testBuildExecutorStatus() {
+        getDriver().findElement(By.xpath("//a[text()='Build Executor Status']")).click();
+        getDriver().findElement(By.className("jenkins-button")).click();
+        final String text = "New node name";
+        getDriver().findElement(By.cssSelector("input[id = 'name']")).sendKeys(text);
+        getDriver().findElement(By.className("jenkins-radio__label")).click();
+        getDriver().findElement(By.cssSelector("#ok")).click();
+        getDriver().findElement(By.cssSelector("button[name='Submit']")).click();
+        WebElement ManageNodes = getDriver().findElement(By.xpath("//h1[text() = 'Manage nodes and clouds']"));
+        Assert.assertEquals(ManageNodes.getText(),"Manage nodes and clouds");
+    }
 }
