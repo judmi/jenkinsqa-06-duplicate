340ab72a8ddf5b07f81f5c863f68e4343363e347
JuliaNuga <130059145+JuliaNuga@users.noreply.github.com>
2023-04-30 12:55:04 +0200

    Qa rabbit hole1 (#1259)
    * Update QaRabbitHoleTest.java
    
    ---------
    
    Co-authored-by: Sergey Kuznetsov <votkinsk@gmail.com>

--- src/test/java/school/redrover/QaRabbitHoleTest.java
+++ src/test/java/school/redrover/QaRabbitHoleTest.java
@@ -6,13 +6,18 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class QaRabbitHoleTest extends BaseTest {
-    @Test
-    public void testWelcomeJenkins(){
 
+    @Test
+    public void testWelcomeJenkins() {
         WebElement h1WelcomeJenkins = getDriver().findElement(By.cssSelector(".empty-state-block>h1"));
 
         Assert.assertEquals(h1WelcomeJenkins.getText(), "Welcome to Jenkins!");
     }
+
+    @Test
+    public void testDashboard() {
+        WebElement dashboard = getDriver().findElement((By.xpath("//li/a[@class='model-link']")));
+
+        Assert.assertEquals(dashboard.getText(), "Dashboard");
+    }
 }
-
-
