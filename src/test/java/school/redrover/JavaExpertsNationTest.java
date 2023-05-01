3afb5b3233082d44afe4582930e2427dd220516d
Anastasiya Lugovskaya <121308236+AnastasiyaLugovskaya@users.noreply.github.com>
2023-05-01 16:23:41 +0300

    Al (#1291)
    * fix: add waiting time into testAddDescription
    
    * feat: add 2 tests - testJenkins2_387_2Link and testCreateNewFolder

--- src/test/java/school/redrover/JavaExpertsNationTest.java
+++ src/test/java/school/redrover/JavaExpertsNationTest.java
@@ -2,9 +2,15 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JavaExpertsNationTest extends BaseTest {
     @Test
@@ -45,6 +51,9 @@ public class JavaExpertsNationTest extends BaseTest {
     public void testAddDescription(){
         WebElement addDescLink = getDriver().findElement(By.id("description-link"));
         addDescLink.click();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("textarea[name = 'description']"))
+        );
 
         WebElement inputField = getDriver().findElement(By.cssSelector("textarea[name = 'description']"));
         inputField.clear();
@@ -76,5 +85,48 @@ public class JavaExpertsNationTest extends BaseTest {
         WebElement projectTitle = getDriver().findElement(By.tagName("h1"));
 
         Assert.assertEquals(projectTitle.getText(), "Project " + firstJob, "projectTitle does not match inputted text");
+    }
+
+    @Test
+    public void testJenkins2_387_2Link(){
+        WebElement jenkinsLink = getDriver().findElement(By.xpath("//a[@href='https://www.jenkins.io/']"));
+        String expectedURL = jenkinsLink.getAttribute("href");
+        jenkinsLink.click();
+
+        List<String> tabs = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(tabs.get(1));
+        new WebDriverWait(getDriver(), Duration.ofSeconds(3)).until(
+                ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
+
+        String newUrl = getDriver().getCurrentUrl();
+        getDriver().switchTo().window(getDriver().getWindowHandle()).close();
+
+        Assert.assertEquals(newUrl, expectedURL);
+    }
+    @Test
+    public void testCreateNewFolder(){
+        WebElement newItemButton = getDriver().findElement(
+                By.xpath("//a[@href='/view/all/newJob']"));
+        newItemButton.click();
+
+        WebElement inputField = getDriver().findElement(By.id("name"));
+        inputField.clear();
+        String folderName = "My first folder";
+        inputField.sendKeys(folderName);
+
+        WebElement folderButton = getDriver().findElement(
+                By.xpath("//*[@class='com_cloudbees_hudson_plugins_folder_Folder']/div[@class='desc']"));
+        folderButton.click();
+
+        getDriver().findElement(By.id("ok-button")).click();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+
+        WebElement saveButton = getDriver().findElement(By.cssSelector("button[name='Submit']"));
+        saveButton.click();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+
+        String actualFolderName = getDriver().findElement(By.tagName("h1")).getText();
+
+        Assert.assertEquals(actualFolderName, folderName);
     }
 }
