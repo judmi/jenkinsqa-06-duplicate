e2ca80237aa91ea5d963edba86bf9b22b3b2ecf8
Anna Morozova <morozanna503@gmail.com>
2023-05-04 20:47:42 +0400

    added new testAddedDescriptionToMultiConfiguration (#1353)
    * added new testAddedDescriptionToMultiConfiguration
    
    * added new testAddedDescriptionToMultiConfiguration

--- src/test/java/school/redrover/MultiConfigurationTest.java
+++ src/test/java/school/redrover/MultiConfigurationTest.java
@@ -1,4 +1,5 @@
 package school.redrover;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -8,6 +9,9 @@ import school.redrover.runner.BaseTest;
 
 
 public class MultiConfigurationTest extends BaseTest {
+    private static final String MULTICONFIGURATION_NAME = RandomStringUtils.randomAlphanumeric(5);
+    private static final String DESCRIPTION = "Description";
+    private static final String DESCRIPTION_RANDOM = RandomStringUtils.randomAlphanumeric(5);
     @Test
     public void testCreateMultiConfig() {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
@@ -21,13 +25,27 @@ public class MultiConfigurationTest extends BaseTest {
     }
 
     @Test
-    public void testCheckExceptionToMultiConfigurationPage() throws InterruptedException {
+    public void testCheckExceptionToMultiConfigurationPage() {
         getDriver().findElement(By.linkText("New Item")).click();
-        WebElement element = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Multi-configuration project']")));
+        WebElement element = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Multi-configuration project']")));
         element.click();
         String exceptionText = getDriver().findElement(By.xpath("//div[text() ='» This field cannot be empty, please enter a valid name']")).getText();
 
         Assert.assertEquals(exceptionText,"» This field cannot be empty, please enter a valid name");
+    }
+
+    @Test
+    public void testAddedDescriptionToMultiConfiguration() {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(MULTICONFIGURATION_NAME);
+        WebElement projectButton = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Multi-configuration project']")));
+        projectButton.click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.name("description")).sendKeys(DESCRIPTION);
+        getDriver().findElement(By.cssSelector("[name='Submit']")).click();
+        WebElement descriptionText = getDriver().findElement(By.cssSelector("div#description"));
+
+        Assert.assertEquals(descriptionText.getText().trim().substring(0, 11),"Description" );
     }
 
 }
