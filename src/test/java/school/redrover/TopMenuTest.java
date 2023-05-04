667bede02c33f1e8f38e5699bdf07370277f82ea
Anzhelika Baaklini <anzhelikb@gmail.com>
2023-05-04 16:58:04 -0400

    testPreviewOfAddedDescriptionWhenClickUserIDButton() added.

--- src/test/java/school/redrover/TopMenuTest.java
+++ src/test/java/school/redrover/TopMenuTest.java
@@ -198,4 +198,27 @@ public class TopMenuTest extends BaseTest {
         Assert.assertEquals(actualNewProjectName,expectedNewProjectName);
 
     }
+
+    @Test
+    public void testPreviewOfAddedDescriptionWhenClickUserIDButton() {
+
+        String expectedPreviewOfAddedDescription = "QA Engineer";
+
+        WebElement userIDButton = getDriver().findElement(By.xpath("//div[@class=\"login page-header__hyperlinks\"]/a[1]/span"));
+        userIDButton.click();
+
+        WebElement addDescriptionButton = getDriver().findElement(By.xpath("//a[@id=\'description-link\']"));
+        addDescriptionButton.click();
+
+        WebElement textArea = getDriver().findElement(By.xpath("//div[@class=\'setting-main help-sibling\']/textarea"));
+        textArea.sendKeys("QA Engineer");
+
+        WebElement previewButton = getDriver().findElement(By.xpath("//div[@class=\'textarea-preview-container\']/a[@class=\'textarea-show-preview\']"));
+        previewButton.click();
+
+        WebElement previewDescription = getDriver().findElement(By.xpath("//div[@class=\'textarea-preview\']"));
+        String actualPreviewOfAddedDescription = String.valueOf(previewDescription.getText());
+
+        Assert.assertEquals(actualPreviewOfAddedDescription, expectedPreviewOfAddedDescription);
+    }
 }
