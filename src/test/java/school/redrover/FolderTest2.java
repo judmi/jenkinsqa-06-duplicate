fe5180f455b528c3bb0039956d5f22479f0a3c84
ReginaKurtLV <128086386+ReginaKurtLV@users.noreply.github.com>
2023-05-06 16:28:49 -0700

    AT 04.02| New item > Folder > Create and Rename Folder (#1423)
    https://trello.com/c/J050Gjmn

--- src/test/java/school/redrover/FolderTest2.java
+++ src/test/java/school/redrover/FolderTest2.java
@@ -14,12 +14,19 @@ public class FolderTest2 extends BaseTest {
     private static final By DISPLAY_NAME = By.name("_.displayNameOrNull");
     private static final By DESCRIPTION = By.name("_.description");
     private static final By SUBMIT = By.name("Submit");
+    private static final By FOLDER_PAGE = By.cssSelector("div[id='main-panel'] h1");
+    private static final By FOLDER_DASHBOARD = By.xpath("//a[@class='jenkins-table__link model-link inside']//span");
+    private  static final By DASHBOARD = By.xpath("//a[normalize-space()='Dashboard']");
+
+    private static  final By CONFIGURE = By.xpath("//a[@href='/job/newProject/configure']");
+
+    String name = "newProject";
+    String displayNameText = "NewTest";
+    String descriptionText = "Test project to QA Redrover.school";
+    String renameText = "FolderOne";
 
     @Test
-    public void testCreateNewItemFolder() {
-        String name = "newProject";
-        String displayNameText = "NewTest";
-        String descriptionText = "Test project to QA Redrover.school";
+    public void testCreateNewItemFolderAndRenameFolder() {
 
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(NAME).sendKeys(name);
@@ -28,7 +35,16 @@ public class FolderTest2 extends BaseTest {
         getDriver().findElement(DISPLAY_NAME).sendKeys(displayNameText);
         getDriver().findElement(DESCRIPTION).sendKeys(descriptionText);
         getDriver().findElement(SUBMIT).click();
+        getDriver().findElement(DASHBOARD).click();
+        getDriver().findElement(FOLDER_DASHBOARD).click();
 
-        Assert.assertEquals("NewTest", getDriver().findElement(By.cssSelector("div[id='main-panel'] h1")).getText());
+        Assert.assertEquals(displayNameText, getDriver().findElement(FOLDER_PAGE).getText());
+
+        getDriver().findElement(CONFIGURE).click();
+        getDriver().findElement(DISPLAY_NAME).clear();
+        getDriver().findElement(DISPLAY_NAME).sendKeys(renameText);
+        getDriver().findElement(SUBMIT).click();
+
+        Assert.assertEquals(renameText, getDriver().findElement(FOLDER_PAGE).getText());
     }
 }
