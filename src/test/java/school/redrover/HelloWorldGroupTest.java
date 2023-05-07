648b66b1d6099ff635953af80b3fa15449768d18
Olga Kovalenko <89601793+Olgamalyuta@users.noreply.github.com>
2023-05-07 13:00:39 +0300

    AT 04.01 | New item > Folder > Create Folder (#1433)
    * AT 04.01 | New item > Folder > Create Folder
    
    * AT 04.01 | New item > Folder > Create Folder
    added empty line

--- src/test/java/school/redrover/HelloWorldGroupTest.java
+++ src/test/java/school/redrover/HelloWorldGroupTest.java
@@ -9,31 +9,6 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class HelloWorldGroupTest extends BaseTest{
-    @Test
-    public void testCreateFilder() {
-        WebElement item = getDriver().findElement(By.xpath("//*[@id=\"tasks\"]//span/a"));
-        item.click();
-
-        WebElement itemName = getDriver().findElement(By.xpath("//input[@name='name']"));
-        itemName.sendKeys("Name");
-
-        WebElement folder =getDriver().findElement(By.xpath("//*[@id=\"j-add-item-type-nested-projects\"]//div[@class='desc']"));
-        folder.click();
-
-        WebElement okButton = getDriver().findElement(By.xpath("//*[@id=\"ok-button\"]"));
-        okButton.click();
-
-        WebElement saveButton = getDriver().findElement(By.xpath("//*[@id=\"bottom-sticker\"]//button[1]"));
-        saveButton.click();
-
-        WebElement dashboard = getDriver().findElement(By.xpath("//*[@id=\"breadcrumbs\"]//a"));
-        dashboard.click();
-
-        WebElement nameFolder = getDriver().findElement(By.xpath("//td//a//span[1]"));
-
-        Assert.assertEquals(nameFolder.getText(),"Name");
-    }
-
     @Ignore
     @Test
     public void testUserName(){
