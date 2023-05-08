ad6d3ea6617633638d85e373e26346e064fad707
Uladzimir Tarasavets <77638965+VladimirTarasovets@users.noreply.github.com>
2023-05-08 19:52:58 +0300

    AT 13.02 | Footer > Verify REST API link (#1477)

--- src/test/java/school/redrover/FooterTest2.java
+++ src/test/java/school/redrover/FooterTest2.java
@@ -10,6 +10,8 @@ public class FooterTest2 extends BaseTest {
     private static final By JENKINS_VERSION_BTN = By.xpath("//a[@rel='noopener noreferrer']");
     private static final By MANAGE_JENKINS_BTN = By.xpath("//span[contains(text(),'Manage Jenkins')]/..");
     private static final String JENKINS_VERSION = "Jenkins 2.387.2";
+    private static final By REST_API_BTN = By.xpath("//a[contains(text(),'REST API')]");
+    private static final String TITLE = "Remote API [Jenkins]";
 
     @Test
     public void testVerifyJenkinsVersionOnManageJenkinsPage() {
@@ -19,6 +21,15 @@ public class FooterTest2 extends BaseTest {
         actions.scrollToElement(getDriver().findElement(JENKINS_VERSION_BTN));
 
         Assert.assertEquals(getDriver().findElement(JENKINS_VERSION_BTN).getText(), JENKINS_VERSION, "Wrong version Jenkins");
+    }
+
+    @Test
+    public void testVerifyRESTAPILink(){
+        Actions actions = new Actions(getDriver());
+        actions.scrollToElement(getDriver().findElement(REST_API_BTN));
+
+        getDriver().findElement(REST_API_BTN).click();
+        Assert.assertEquals(getDriver().getTitle(),TITLE, "Wrong page or page title");
     }
 
 }
