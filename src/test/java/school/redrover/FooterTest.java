a761a02fe702d023d6f7ee9708e029f898d9cbab
Aliaksei Ramanouski <69923443+MerseysideRed@users.noreply.github.com>
2023-05-08 09:01:38 +0300

    AT 13.01 | Footer > Check Jenkins version on Footer in People page (#1453)
    * TC 13.01 | Footer > Check Jenkins version on Footer in People page
    
    * Ignore testNewMultiConfigurationProject

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -2,7 +2,9 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -32,5 +34,15 @@ public class FooterTest extends BaseTest {
     public void testVerifyJenkinsVersion() {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='https://www.jenkins.io/']")).getText(), "Jenkins 2.387.2");
+    }
+
+    @Test
+    public void testCheckJenkinsVersionInPeoplePage(){
+        WebElement peoplePageButton = getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']"));
+        peoplePageButton.click();
+
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(),'People')]")));
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@rel='noopener noreferrer']")).getText(),"Jenkins 2.387.2");
     }
 }
