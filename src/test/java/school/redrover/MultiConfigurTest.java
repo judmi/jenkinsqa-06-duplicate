9219177d831843b0bcf9cfbd3ae66f04b6d8a68c
Olga Kovalenko <89601793+Olgamalyuta@users.noreply.github.com>
2023-05-08 08:51:43 +0300

    AT 03.01 | Multi-Configuration project > add description (#1436)

--- src/test/java/school/redrover/MultiConfigurTest.java
+++ src/test/java/school/redrover/MultiConfigurTest.java
@@ -8,16 +8,37 @@ import school.redrover.runner.BaseTest;
 
 public class MultiConfigurTest extends BaseTest {
     private static final String NAME_OF_PROJECT = "New project";
-    @Test
-    public void createMultiConfigurationProjectTest() {
+    private static final String DESCRIPTION ="Description";
+
+
+    private void createBaseMultiConfigurationProject() {
         getDriver().findElement(By.xpath("//*[@id='tasks']//span/a")).click();
-        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(NAME_OF_PROJECT);
+        getDriver().findElement(By.name("name")).sendKeys(NAME_OF_PROJECT);
         getDriver().findElement(By.xpath("//label//span[text() ='Multi-configuration project']")).click();
         getDriver().findElement(By.xpath("//div[@class ='btn-decorator']")).click();
-        getDriver().findElement(By.xpath("//*[@id='breadcrumbs']//a")).click();
+        }
+
+    @Test
+    public void createMultiConfigurationProjectTest() {
+        createBaseMultiConfigurationProject();
+
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.linkText("Dashboard")).click();
 
         WebElement nameMultifigurationProject = getDriver().findElement(By.xpath("//td//a//span[1]"));
 
         Assert.assertEquals(nameMultifigurationProject.getText(),NAME_OF_PROJECT);
+    }
+
+    @Test
+    public void createMultiConfigurationProjectWithDescriptionTest() {
+        createBaseMultiConfigurationProject();
+
+        getDriver().findElement(By.name("description")).sendKeys(DESCRIPTION);
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement nameDescription = getDriver().findElement(By.xpath("//div[@id ='description']//div"));
+
+        Assert.assertEquals(nameDescription.getText(),DESCRIPTION);
     }
 }
