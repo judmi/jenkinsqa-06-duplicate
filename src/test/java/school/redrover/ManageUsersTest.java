8d95d46cacf5246fb297a4e09d148a3ff5ee4bd3
KathyGU <97336394+KathyGU@users.noreply.github.com>
2023-05-09 00:57:02 +0300

    AT 09.03 | Manage Jenkins > Manage Users > Delete User (#1499)

--- src/test/java/school/redrover/ManageUsersTest.java
+++ src/test/java/school/redrover/ManageUsersTest.java
@@ -22,6 +22,15 @@ public class ManageUsersTest extends BaseTest {
     private static final By FULL_NAME_FIELD = By.xpath("//input[@name='fullname']");
     private static final By EMAIL_FIELD = By.xpath("//input[@name='email']");
     private static final By CREATE_USER_BUTTON = By.xpath("//button[@name='Submit']");
+    private static final By LOG_OUT_BUTTON = By.xpath("//a[@href = '/logout']");
+    private static final By LOG_IN_USERNAME_FIELD = By.id("j_username");
+    private static final By LOG_IN_PASSWORD_FIELD = By.xpath("//input[@name='j_password']");
+    private static final By SUBMIT_BUTTON = By.xpath("//button[@name='Submit']");
+
+    public static final String username = "Mr_Churchill";
+    public static final String password = "Churchill2023";
+
+
 
     @Test
     public void testDeleteUserFromUserList() {
@@ -29,19 +38,30 @@ public class ManageUsersTest extends BaseTest {
         getDriver().findElement(MANAGE_JENKINS).click();
         getDriver().findElement(MANAGE_USERS).click();
         getDriver().findElement(By.xpath("//a[@href='user/mr_churchill/delete']")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(SUBMIT_BUTTON).click();
 
         List<WebElement> UserNames = getDriver().findElements(By.xpath("//a[contains(@class, 'link model-link inside')]"));
 
         Assert.assertFalse(isUserDisplayed(UserNames, "Churchill"));
     }
+    @Test (dependsOnMethods = "testDeleteUserFromUserList")
+    public void testLogInWithDeletedUserCredentialsImpossible() {
+        getDriver().findElement(LOG_OUT_BUTTON).click();
+        getDriver().findElement(LOG_IN_USERNAME_FIELD).sendKeys(username);
+        getDriver().findElement(LOG_IN_PASSWORD_FIELD).sendKeys(password);
+        getDriver().findElement(SUBMIT_BUTTON).click();
+
+        Assert.assertEquals(getDriver().findElement(By
+                .xpath("//div[@class ='alert alert-danger']")).getText(),
+                "Invalid username or password");
+    }
     public void createUser() {
         getDriver().findElement(MANAGE_JENKINS).click();
         getDriver().findElement(MANAGE_USERS).click();
         getDriver().findElement(CREATE_USER).click();
-        getDriver().findElement(USERNAME_FIELD).sendKeys("Mr_Churchill");
-        getDriver().findElement(PASSWORD_FIELD).sendKeys("Churchill2023");
-        getDriver().findElement(CONFIRM_PASSWORD_FIELD).sendKeys("Churchill2023");
+        getDriver().findElement(USERNAME_FIELD).sendKeys(username);
+        getDriver().findElement(PASSWORD_FIELD).sendKeys(password);
+        getDriver().findElement(CONFIRM_PASSWORD_FIELD).sendKeys(password);
         getDriver().findElement(FULL_NAME_FIELD).sendKeys("Churchill");
         getDriver().findElement(EMAIL_FIELD).sendKeys("test@test.com");
         getDriver().findElement(CREATE_USER_BUTTON).click();
