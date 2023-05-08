49112ba86c99a65485cf06b028d4788c6c284bf4
NadiaSergeeva <130238321+NadiaSergeeva@users.noreply.github.com>
2023-05-08 09:32:28 +0100

    Nasergeeva/rf0401 (#1452)
    RF 04.01| New item > Folder > Create Folder without configuration
    https://trello.com/c/c7a0ICuX

--- src/test/java/school/redrover/FolderTest3.java
+++ src/test/java/school/redrover/FolderTest3.java
@@ -6,26 +6,59 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import java.util.Arrays;
+import java.util.List;
 
 public class FolderTest3 extends BaseTest {
-    public void createBaseFolder () {
+    private static final By NAME=By.name("name");
+    public void fillFolderNameField (String name) {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getWait2().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"))));
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
 
-        WebElement nameinput=getDriver().findElement(By.name("name"));
-        getWait2().until(ExpectedConditions.visibilityOf(nameinput));
-        nameinput.sendKeys("folder1");
+        WebElement nameinput=getDriver().findElement(NAME);
+        getWait5().until(ExpectedConditions.visibilityOf(nameinput));
+        nameinput.sendKeys(name);
+    }
 
+    public void createBaseFolder (String name) {
+        fillFolderNameField(name);
         getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']")).click();
         WebElement okbutton = getDriver().findElement(By.id("ok-button"));
         okbutton.click();
-
-        getWait2().until(ExpectedConditions.textToBe(By.xpath("//h1[text()='Configuration']"), "Configuration"));
     }
-    @Test
-    public void testCreateFolder (){
-        createBaseFolder();
 
-        getDriver().findElement(By.xpath("//a[normalize-space()='folder1']")).click();
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='folder1']")).getText(),"folder1");
-    }
+        @Test
+        public void testCreateFolderPositive (){
+            List<String> positivevalues = Arrays.asList("Folder1", " spaces ", "123");
+             for (String value : positivevalues) {
+                createBaseFolder(value);
+                value = value.trim();
+                getWait2().until(ExpectedConditions.textToBe(By.xpath("//h1[text()='Configuration']"), "Configuration"));
+                getDriver().findElement(By.xpath("//a[normalize-space()='"+value+"']")).click();
+
+                Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='"+value+"']")).getText(),value);
+            }
+        }
+
+        @Test
+        public void testCreateFolderNegative () {
+            List<String> negativevalues = Arrays.asList(" ");
+            for (String value : negativevalues) {
+                createBaseFolder(value);
+                Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Error");
+            }
+        }
+
+        @Test
+        public void testCreateFolderSpecSymbols () {
+            List<String> specvalues = Arrays.asList("@", "#", "$", "%", "^", "&");
+            for (String value : specvalues) {
+                fillFolderNameField(value);
+                Assert.assertEquals(getDriver().findElement(By.id("itemname-invalid")).getText(), "» ‘"+value+"’ is an unsafe character");
+                WebElement nameinput=getDriver().findElement(NAME);
+                nameinput.clear();
+            }
+        }
+
 }
