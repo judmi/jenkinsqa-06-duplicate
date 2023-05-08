e0ceb2b9988361fc9b28f3a654fda134b4e045f1
OlgaMinina <arb.test2020@gmail.com>
2023-05-08 16:18:56 +0200

    AT 15.01 | Users > Create users

--- src/test/java/school/redrover/UserTest.java
+++ src/test/java/school/redrover/UserTest.java
@@ -39,7 +39,7 @@ import school.redrover.runner.BaseTest;
             WebElement createUserButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
             createUserButton.click();
 
-            WebElement createdUser = getDriver().findElement(By.xpath("//td[contains(text(),'admin')]"));
+            WebElement createdUser = getDriver().findElement(By.xpath("(//a[normalize-space()='admin'])[1]"));
             Assert.assertEquals(createdUser.getText(), USERDATA);
         }
     }
