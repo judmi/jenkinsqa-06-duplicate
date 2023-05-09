eeafe8d51b4e15d504cb2fdc1ed0969d2082cdd7
Ulyana Gennadevna <uveryaskina@gmail.com>
2023-05-09 20:55:43 +0300

    At 00.02 new item create pipeline project (#1542)
    * add my test logout2
    
    * Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Вторая попытка отправить:
    Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Maven проблемма
    TC 00.02 | New Item > Create Pipeline Project
    
    * AT 00.02 | New Item > Create Pipeline Project
    
    * AT 00.02 | New Item > Create Pipeline Project
    
    * AT 00.02 | New Item > Create Pipeline Project
    
    * AT 00.02 | New Item > Create Pipeline Project
    Delele 5 вариант лишний

---
+++ src/test/java/school/redrover/CreatePipelineProject7Test.java
@@ -0,0 +1,30 @@
+package school.redrover;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+    public class CreatePipelineProject7Test extends BaseTest {
+        private By NEW_ITEM = By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']");
+
+        private By INPUT_NAME = By.xpath("//div//input[@name = 'name'][@id ='name']");
+        private By CREATE_PIPELINE = By.xpath(
+                "//img[@src='/plugin/workflow-job/images/pipelinejob.svg']");
+        private By OK_BUTTON = By.id("ok-button");
+
+        private By SAVE_BUTTON = By.name("Submit");
+
+        @Test
+
+        public void testCreatePipelineProjectNewItem() {
+            getDriver().findElement(NEW_ITEM).click();
+            getDriver().findElement(INPUT_NAME).sendKeys("Project1");
+            getDriver().findElement(CREATE_PIPELINE).click();
+            getDriver().findElement(OK_BUTTON).click();
+            getDriver().findElement(SAVE_BUTTON).click();
+
+            Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id = 'main-panel']//h1['Pipeline Project1']")).getText(),
+                    "Pipeline Project1");
+        }
+}
+
