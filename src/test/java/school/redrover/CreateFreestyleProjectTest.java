99411c8ddb133ed898a9c961d809a56ac3be17c3
Alex Evtuh <Alexander.evtukh@yandex.ru>
2023-05-09 16:50:47 +0300

    AT 00.01 | New Item > Freestyle Project > Create Freestyle Project with invalid name

--- src/test/java/school/redrover/CreateFreestyleProjectTest.java
+++ src/test/java/school/redrover/CreateFreestyleProjectTest.java
@@ -1,6 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -26,4 +28,24 @@ public class CreateFreestyleProjectTest extends BaseTest {
 
         Assert.assertEquals(actualProjectName, expectedProjectName);
     }
+
+    @Test
+    public void testCreateFreestyleProjectWithInvalidName() {
+        getDriver().findElement(By.xpath("//a[contains(@href, 'newJob')]")).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//li[contains(@class, 'FreeStyleProject')]/label"))).click();
+
+        WebElement inputField = getDriver().findElement(By.xpath("//input[@id='name']"));
+
+        String[] invalidSymbols = {"!", "@", "#", "$", "%", "^", "&", "*", ":", ";", "/", "|", "?", "<", ">"};
+        for (String invalidSymbol : invalidSymbols) {
+            inputField.sendKeys(invalidSymbol);
+            String validationMessage = getDriver().findElement(By.xpath("//div[@id='itemname-invalid']")).getText();
+            inputField.clear();
+
+            Assert.assertEquals(validationMessage, "» ‘" + invalidSymbol + "’ is an unsafe character");
+            Assert.assertFalse(getDriver().findElement(By.xpath("//button[@id='ok-button']")).isEnabled());
+        }
+    }
+
 }
