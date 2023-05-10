b32590e110c6c2342def5d1de0bd09d3d751c9c0
EleonoraButonaeva <130123558+EleonoraButonaeva@users.noreply.github.com>
2023-05-10 20:36:07 +0700

    AT 00.01 | New Item > Freestyle Project > Create Freestyle Project (#1483)
    * add testAddDescription
    
    * delete testAddDescription and add testCreateUser
    
    * AT 00.01 | New Item > Freestyle Project > Create Freestyle Project (add testNewProject and delete it in NeedMoreCoffeeTest class)
    
    * AT 00.01 | New Item > Freestyle Project > Create Freestyle Project (rename class)

--- src/test/java/school/redrover/NeedMoreCoffeeTest.java
+++ src/test/java/school/redrover/NeedMoreCoffeeTest.java
@@ -28,25 +28,6 @@ public class NeedMoreCoffeeTest extends BaseTest {
     }
 
     @Test
-    public void testNewProject() {
-
-        WebElement newItem = getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a"));
-        newItem.sendKeys(Keys.RETURN);
-        WebElement field = getDriver().findElement(By.xpath("//*[@id=\"name\"]"));
-        field.sendKeys("project");
-        WebElement freestyleProject = getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]"));
-        freestyleProject.sendKeys(Keys.RETURN);
-        WebElement tabOk = getDriver().findElement(By.xpath("//*[@id=\"ok-button\"]"));
-        tabOk.sendKeys(Keys.RETURN);
-        WebElement tabSave = getDriver().findElement(By.xpath("//*[@id=\"bottom-sticker\"]/div/button[1]"));
-        tabSave.sendKeys(Keys.RETURN);
-        WebElement textProject = getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/h1"));
-
-        Assert.assertEquals(textProject.getText(), "Project project");
-
-    }
-
-    @Test
     public void testNewProjectWithGitHub() {
 
         WebElement createAJob = getDriver().findElement(By.xpath("//span[normalize-space()='Create a job']"));
