ae9f384777b031a292ff200d86065c5b3a1e9812
Victoria_Lem <115832814+VictoriaLema@users.noreply.github.com>
2023-05-10 15:37:56 +0300

    RF 04.01 | New Item > Folder >Create folder (#1566)
    method createAFolder refactored

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -16,31 +16,33 @@ public class Folder2Test extends BaseTest {
     private static final By NAME_FIELD = By.id("name");
     private static final By FOLDER_TYPE = By.cssSelector(".com_cloudbees_hudson_plugins_folder_Folder");
     private static final By OK_BUTTON = By.id("ok-button");
-    private static final By DESCRIPTION_FIELD = By.xpath("//textarea[@name='_.description']");
     private static final By SAVE_BUTTON = By.name("Submit");
     private static final By FOLDER_HEADER = By.tagName("h1");
     private static final By DASHBOARD_LINK = By.xpath("//a[@href='/'][@class='model-link']");
     private static final By FOLDER_IN_LIST = By.xpath("//a[@class='jenkins-table__link model-link inside']/span");
     private static final By JOB_HEADER = By.tagName("h1");
 
-    public void createAFolder(String name, String description) {
+    private void createAFolder(String name) {
         getDriver().findElement(NEW_ITEM).click();
+
         getWait10().until(ExpectedConditions.presenceOfElementLocated(NAME_FIELD));
+
         getDriver().findElement(NAME_FIELD).sendKeys(name);
         getDriver().findElement(FOLDER_TYPE).click();
         getDriver().findElement(OK_BUTTON).click();
+
         getWait5().until(ExpectedConditions.elementToBeClickable(SAVE_BUTTON));
-        getDriver().findElement(DESCRIPTION_FIELD).sendKeys(description);
+
         getDriver().findElement(SAVE_BUTTON).click();
+
         getWait2().until(ExpectedConditions.textToBe(FOLDER_HEADER,name));
     }
 
     @Test
     public void testFolderCreation() {
         final String FOLDER_NAME = "My folder";
-        final String DESCRIPTION = "";
 
-        createAFolder(FOLDER_NAME,DESCRIPTION);
+        createAFolder(FOLDER_NAME);
 
         getDriver().findElement(DASHBOARD_LINK).click();
 
@@ -56,19 +58,17 @@ public class Folder2Test extends BaseTest {
     public void testTwoFoldersCreation() {
         final String FOLDER1_NAME = "My main folder";
         final String FOLDER2_NAME = "My folder";
-        final String DESCRIPTION = "";
         List<String> expectedFoldersList = Arrays.asList(FOLDER1_NAME, FOLDER2_NAME);
 
-        createAFolder(FOLDER1_NAME,DESCRIPTION);
+        createAFolder(FOLDER1_NAME);
         getDriver().findElement(DASHBOARD_LINK).click();
 
         getWait10().until(ExpectedConditions.presenceOfElementLocated(NEW_ITEM));
 
-        createAFolder(FOLDER2_NAME,DESCRIPTION);
+        createAFolder(FOLDER2_NAME);
         getDriver().findElement(DASHBOARD_LINK).click();
 
-        List <WebElement> actualFoldersList = getDriver().findElements(By.xpath(
-                "//td/a[@class='jenkins-table__link model-link inside']/span"));
+        List <WebElement> actualFoldersList = getDriver().findElements(By.xpath("//td/a[@class='jenkins-table__link model-link inside']/span"));
 
         for (int i = 0; i < actualFoldersList.size(); i++) {
 
