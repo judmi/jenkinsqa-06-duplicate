31e3bfd8ac5ca68db04fbcb43e1cb2a5abd6e66e
Artem Savichev <90255887+SavaSavichev@users.noreply.github.com>
2023-05-10 23:03:38 +0600

    AT 02.05 add delete pipeline test (#1579)

--- src/test/java/school/redrover/DeletePipeline1Test.java
+++ src/test/java/school/redrover/DeletePipeline1Test.java
@@ -1,12 +1,11 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class CreatePipelineProject3Test extends BaseTest {
+public class DeletePipeline1Test extends BaseTest {
     public static String name = "My New Pipeline Project";
     public static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
     public static final By INPUT_NAME = By.id("name");
@@ -15,33 +14,23 @@ public class CreatePipelineProject3Test extends BaseTest {
     public static final By SAVE_BUTTON = By.xpath("//button[normalize-space()='Save']");
     public static final By JENKINS_LOGO = By.id("jenkins-name-icon");
     public static final By PROJECT_IN_DASHBOARD_TABLE = By.xpath("//span[normalize-space()='" + name + "']");
+    public static final By DELETE_BUTTON = By.xpath("//span[contains(text(),'Delete Pipeline')]");
+    public static final By MAIN_PAGE_MESSAGE = By.xpath("//h1[normalize-space()='Welcome to Jenkins!']");
 
     @Test
-    public void testCreatePipelineProject() {
+    public void testDeletePipelineProject() {
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(INPUT_NAME).sendKeys(name);
         getDriver().findElement(PIPELINE_PROJECT_TYPE).click();
         getDriver().findElement(OK_BUTTON).click();
         getDriver().findElement(SAVE_BUTTON).click();
+
         getDriver().findElement(JENKINS_LOGO).click();
-
-        Assert.assertEquals(getDriver().findElement(PROJECT_IN_DASHBOARD_TABLE).getText(), name);
-
         getDriver().findElement(PROJECT_IN_DASHBOARD_TABLE).click();
 
-        Assert.assertEquals(getDriver().findElement(
-                By.xpath("//h1[normalize-space()='Pipeline " + name + "']"))
-                .getText(), "Pipeline " + name);
-    }
+        getDriver().findElement(DELETE_BUTTON).click();
+        getDriver().switchTo().alert().accept();
 
-    @Test
-    public void testPipelineNameUnsafeChar() {
-        getDriver().findElement(NEW_ITEM).click();
-        getDriver().findElement(INPUT_NAME).sendKeys("*&^%$#@!");
-        getWait2();
-        getDriver().findElement(PIPELINE_PROJECT_TYPE).click();
-        WebElement message = getDriver().findElement(By.xpath("//div[@id='itemname-invalid']"));
-
-        Assert.assertEquals (message.getText(), "» ‘*’ is an unsafe character");
+        Assert.assertEquals(getDriver().findElement(MAIN_PAGE_MESSAGE).getText(), "Welcome to Jenkins!");
     }
 }
