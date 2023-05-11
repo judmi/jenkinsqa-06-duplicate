caf615d40b15d43c00bd2efeec9a34ff6f74aa7a
ArtyomDulya <111312308+ArtyomDulya@users.noreply.github.com>
2023-05-11 11:10:38 +0500

    AT 01.05.01 | Freestyle project > Preview description (#1624)
    * AT 01.05.01 | Freestyle project > Preview description

--- src/test/java/school/redrover/PreviewDescriptionTest.java
+++ src/test/java/school/redrover/PreviewDescriptionTest.java
@@ -1,16 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.WheelInput;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 
-public class ProjectDescriptionTest extends BaseTest {
+public class PreviewDescriptionTest extends BaseTest {
 
     public void createFreestyleProject() {
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
@@ -22,28 +19,17 @@ public class ProjectDescriptionTest extends BaseTest {
     }
 
     @Test
-    public void testProjectDescription() {
-        String expectedResult = "Test";
+    public void testPreviewDescription() {
+        String expectedResult = "wwwww";
         createFreestyleProject();
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        WebElement description = getDriver().findElement(By.xpath("//a[@id='description-link']"));
-        description.click();
-        String string = "w";
-
-        for(int i = 0; i < 10; i++) {
-            string += string;
-        }
-
-        getDriver().findElement(By.xpath("//textarea[@class='jenkins-input   ']")).sendKeys(string);
-        WebElement submit = getDriver().findElement(By.xpath("//button[@name='Submit']"));
-        submit.click();
-
         getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
         getDriver().findElement(By.xpath("//textarea[@class='jenkins-input   ']")).clear();
-        getDriver().findElement(By.xpath("//textarea[@class='jenkins-input   ']")).sendKeys("Test");
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//textarea[@class='jenkins-input   ']")).sendKeys(expectedResult);
+        getDriver().findElement(By.xpath("//a[text()='Preview']")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(), expectedResult);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='textarea-preview']")).getText(), expectedResult);
+        Assert.assertTrue(getDriver().findElement(By.xpath("//div[@class='textarea-preview']")).isDisplayed());
     }
 }
