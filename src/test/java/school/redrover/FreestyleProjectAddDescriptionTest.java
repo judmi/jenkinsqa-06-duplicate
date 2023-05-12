e6151692322d1d88ea60b7d4dbcaf212d58873e5
Alex Evtuh <Alexander.evtukh@yandex.ru>
2023-05-12 14:42:09 +0300

    Fixes

--- src/test/java/school/redrover/FreestyleProjectAddDescriptionTest.java
+++ src/test/java/school/redrover/FreestyleProjectAddDescriptionTest.java
@@ -10,47 +10,48 @@ import school.redrover.runner.BaseTest;
 
 
 public class FreestyleProjectAddDescriptionTest extends BaseTest {
-
+    private static final By NEW_ITEM_BUTTON = By.xpath("//a[contains(@href, 'newJob')]");
+    private static final By ITEM_NAME_FIELD = By.xpath("//input[@id='name']");
+    private static final By NEW_ITEM_OK_BUTTON = By.xpath("//button[@id='ok-button']");
+    private static final By DASHBOARD_BUTTON = By.xpath("//a[contains(text(), 'Dashboard')]");
     private static final By DESCRIPTION_FIELD = By.xpath("//textarea[@name='description']");
-    private static final By OK_BUTTON = By.xpath("//button[@name='Submit']");
-    private static final By PROJECT_DESCRIPTION = By.xpath("//div[@id='description']/div[1]");
+    private static final By CONFIGURE_SAVE_BUTTON = By.xpath("//button[@name='Submit']");
+    private static final By PROJECT_DESCRIPTION = By.xpath("//div[@id='description']/div[not(contains(@class, 'jenkins'))]");
     private static final By CONFIGURE_OPTION = By.xpath("//span[contains(text(), 'Configure')]/ancestor::a");
 
-    private void createFreestyleProject(String projectName) {
-        getDriver().findElement(By.xpath("//a[contains(@href, 'newJob')]")).click();
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class, 'FreeStyleProject')]/label"))).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(projectName);
-        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-        getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
-    }
 
-    private static String projectNameXpath(String projectName) {
-        return "//span[contains(text(),'" + projectName + "')]/ancestor::a";
+    private void createFreestyleProject(String nameOfProject) {
+        getDriver().findElement(NEW_ITEM_BUTTON).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class, 'FreeStyleProject')]/label"))).click();
+        getDriver().findElement(ITEM_NAME_FIELD).sendKeys(nameOfProject);
+        getDriver().findElement(NEW_ITEM_OK_BUTTON).click();
+        getDriver().findElement(CONFIGURE_SAVE_BUTTON).click();
+        getDriver().findElement(DASHBOARD_BUTTON).click();
     }
 
     @Test
     public void testAddDescriptionToTheProjectViaConfigure() {
-        String nameOfProject = "Leprechaun";
+        final String nameOfProject = "Leprechaun";
         createFreestyleProject(nameOfProject);
 
-        WebElement jobProjectName = getDriver().findElement(By.xpath(projectNameXpath(nameOfProject)));
-        WebElement projectDropdownArrow = getDriver().findElement(By.xpath(projectNameXpath(nameOfProject) + "//button[contains(@class, 'chevron')]"));
+        WebElement jobProjectName = getDriver().findElement(By.xpath("//span[contains(text(),'" + nameOfProject + "')]/ancestor::a"));
+        WebElement projectDropdownArrow = getDriver().findElement(By.xpath("//span[contains(text(),'" + nameOfProject + "')]/ancestor::a//button[contains(@class, 'chevron')]"));
 
         new Actions(getDriver())
                 .moveToElement(jobProjectName)
                 .click(projectDropdownArrow)
-                .build().perform();
+                .perform();
 
         WebElement configureOption = getWait2().until(ExpectedConditions.visibilityOfElementLocated(CONFIGURE_OPTION));
         new Actions(getDriver())
                 .moveToElement(configureOption)
                 .click()
-                .build().perform();
+                .perform();
 
         String descriptionText = "My project description!";
         getDriver().findElement(DESCRIPTION_FIELD).sendKeys(descriptionText);
-        getDriver().findElement(OK_BUTTON).click();
+        getDriver().findElement(CONFIGURE_SAVE_BUTTON).click();
+
         Assert.assertEquals(getDriver().findElement(PROJECT_DESCRIPTION).getText(), descriptionText);
     }
 
