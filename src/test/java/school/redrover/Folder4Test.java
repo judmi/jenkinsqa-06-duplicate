4d26ef5a424524a2e6d8f04f49905df850fd8de0
Mariia Rykova <129385242+RykovaMariia@users.noreply.github.com>
2023-05-12 16:51:11 +0400

    RF 00.04| Item > Create Folder without description (#1725)

--- src/test/java/school/redrover/Folder4Test.java
+++ src/test/java/school/redrover/Folder4Test.java
@@ -9,21 +9,22 @@ import school.redrover.runner.BaseTest;
 
 public class Folder4Test extends BaseTest {
 
+    final String NAME_FOLDER = "Test";
+
     @Test
-    public void testCreateFolder(){
-        final String name = "Test";
+    public void testCreateFolder() throws InterruptedException {
         getDriver().findElement(By.linkText("New Item")).click();
         getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='name']")))
-                .sendKeys(name);
+                .sendKeys(NAME_FOLDER);
         getDriver().findElement(By.cssSelector(".com_cloudbees_hudson_plugins_folder_Folder")).click();
         getWait2().until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
         getDriver().findElement(By.xpath("//button[@name=\"Submit\"]")).click();
+
         getDriver().findElement(By.linkText("Dashboard")).click();
+        getDriver().findElement(By.cssSelector("a[href='job/" + NAME_FOLDER + "/']>span")).click();
 
-        WebElement iconFolder = getDriver().findElement(By.cssSelector("#job_" + name + ">td>div>span"));
-        Assert.assertEquals(iconFolder.getText(),"Folder");
-
-        WebElement nameFolder = getDriver().findElement(By.linkText(name));
-        Assert.assertTrue(nameFolder.isDisplayed(), "error was not shown Folder name");
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("#main-panel>h1")).getText(), NAME_FOLDER);
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("svg[title='Folder']")).isDisplayed(),
+                "error was not shown icon folder");
     }
 }
