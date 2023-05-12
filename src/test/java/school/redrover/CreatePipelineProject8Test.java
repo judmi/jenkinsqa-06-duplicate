9db4fc003cdae180a0926d660e78ada603ac4f0e
KaterynaYazici <katerynayazici7@gmail.com>
2023-05-12 10:47:02 +0300

    AT 00.02 | Create Pipeline Project

--- src/test/java/school/redrover/CreatePipelineProject8Test.java
+++ src/test/java/school/redrover/CreatePipelineProject8Test.java
@@ -11,23 +11,21 @@ import school.redrover.runner.BaseTest;
 import java.time.Duration;
 
 public class CreatePipelineProject8Test extends BaseTest {
-    @Test
-    public void testNewItemSubmit() throws InterruptedException {
-        WebElement newItemBtn = getDriver().findElement(By.cssSelector("a[href='/view/all/newJob']"));
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@class='task-link-text' and .='New Item']")));
-        newItemBtn.click();
 
-        getDriver().findElement(By.cssSelector("#name")).sendKeys("NewName");
-        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+    public static String name = "NewName";
+    public static final By PIPELINE_FINAL_NAME = By.xpath("//h1[contains(text(),'Pipeline " + name + "')]");
+
+    @Test
+    public void testNewItemSubmit() {
+        WebElement newItemBtn = getDriver().findElement(By.cssSelector("a[href='/view/all/newJob']"));
+        newItemBtn.click();
+        getDriver().findElement(By.cssSelector("#name")).sendKeys(name);
+        getDriver().findElement(By.cssSelector(".org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
         getDriver().findElement(By.cssSelector("#ok-button")).click();
-        Thread.sleep(4000);
 
         getDriver().findElement(By.xpath("//button[contains(text(),'Save')]")).click();
-        Thread.sleep(4000);
 
-        WebElement newNodeFileAdded = getDriver().findElement(By.xpath("//h1[contains(text(),'Project NewName')]"));
-        Assert.assertEquals(newNodeFileAdded.getText(),"Project NewName");
+        Assert.assertEquals(getDriver().findElement(PIPELINE_FINAL_NAME).getText(), "Pipeline " + name);
 
     }
 }
