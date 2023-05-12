eb16321afb35b73af92c8fad68b6a49645465716
Rumeantev Veaceslav <slava2045@gmail.com>
2023-05-12 11:41:06 +0300

    https://trello.com/c/TpKeSLqu (#1713)

--- src/test/java/school/redrover/CreateFreestyleProjectTest.java
+++ src/test/java/school/redrover/CreateFreestyleProjectTest.java
@@ -90,4 +90,17 @@ public class CreateFreestyleProjectTest extends BaseTest {
         Assert.assertEquals(projectName.getText(), myProjectName);
     }
 
+    @Test
+    public void testCreateFreestyleProject2(){
+        final String testData = "Test";
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(testData);
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[normalize-space()='Save']")).click();
+
+        WebElement projectPage = getDriver().findElement(By.xpath("//h1[normalize-space()='Project Test']"));
+        Assert.assertEquals(projectPage.getText(), "Project " + testData);
+    }
+
 }
