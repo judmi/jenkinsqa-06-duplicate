f91694ce10fb7612e5e6f72120d5734a5dd999ce
Valeriia_Nikolaieva <shamanmantik0r@gmail.com>
2023-05-12 18:26:07 +0300

    AT 04.05 | Folder > Delete Folder (delete the unnecessary folder and keep other)

--- src/test/java/school/redrover/CreateFolder3Test.java
+++ src/test/java/school/redrover/CreateFolder3Test.java
@@ -1,17 +1,45 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class CreateFolder3Test extends BaseTest {
-    private final String FOLDER_NAME = "New Folder";
+    private static final String FOLDER_NAME = "First Folder";
+    private static final String FOLDER_NAME2 = "Second Folder";
 
     @Test
-    public void testCreateFolder3(){
+    public void testCreateFolder(){
+        createFolder(FOLDER_NAME);
+        WebElement createdFolder = getDriver().findElement(By.xpath("//table[@id='projectstatus']//td//a//span"));
+        Assert.assertEquals(createdFolder.getText(),FOLDER_NAME,"can't find folder name on the dashboard");
+
+        createdFolder.click();
+        Assert.assertTrue(getDriver().getTitle().contains(FOLDER_NAME), "no folder name on the title");
+    }
+
+    @Test(dependsOnMethods = "testCreateFolder")
+    public void testDeleteFolder() {
+        createFolder(FOLDER_NAME2);
+        WebElement dropDownButton = getDriver().findElement(By.xpath("//span[contains(text(),'"+FOLDER_NAME2+"')]/following-sibling::button"));
+        new Actions(getDriver()).moveToElement(dropDownButton).moveToElement(dropDownButton)
+                                .click(dropDownButton).perform();
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//span[normalize-space()='Delete Folder']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//form[@action='doDelete']//button[@name='Submit']"))).click();
+
+        Assert.assertTrue(isElementPresent(By.xpath("//table[@id = 'projectstatus']/descendant::span[contains(text(),'"+FOLDER_NAME+"')]")),
+                                                         "Other folder is not displayed");
+        Assert.assertFalse(isElementPresent(By.xpath("//table[@id = 'projectstatus']/descendant::span[contains(text(),'"+FOLDER_NAME2+"')]")),
+                                                          "Folder is not deleted");
+    }
+
+    private void createFolder(String folderName){
         WebElement newItem = getDriver().findElement(By.xpath("//div[@class = 'task ']//a[@href='/view/all/newJob']"));
         newItem.click();
         getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@class='icon-folder icon-xlg']")));
@@ -19,7 +47,7 @@ public class CreateFolder3Test extends BaseTest {
         WebElement inputField = getDriver().findElement(By.xpath("//input[@class='jenkins-input']"));
         WebElement folderOption = getDriver().findElement(By.xpath("//li[contains(@class,'folder_Folder')]"));
         WebElement okButton = getDriver().findElement(By.xpath("//button[@id='ok-button']"));
-        inputField.sendKeys(FOLDER_NAME);
+        inputField.sendKeys(folderName);
         folderOption.click();
         okButton.click();
 
@@ -27,12 +55,15 @@ public class CreateFolder3Test extends BaseTest {
         saveButton.click();
         WebElement jenkinsLogo = getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']"));
         jenkinsLogo.click();
+    }
 
-        WebElement createdFolder = getDriver().findElement(By.xpath("//table[@id='projectstatus']//span[normalize-space()='New Folder']"));
-        Assert.assertEquals(createdFolder.getText(),FOLDER_NAME,"can't find folder name on the dashboard");
-
-        createdFolder.click();
-        Assert.assertTrue(getDriver().getTitle().contains(FOLDER_NAME), "no folder name on the title");
-
+    public boolean isElementPresent(By by) {
+        try {
+            getDriver().findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
+
