2e49c13c99a13d079839d6d8e9dde18788783fa4
KathyGU <97336394+KathyGU@users.noreply.github.com>
2023-05-12 08:54:18 +0300

    AT 15.03 | Users > User ID > Configure > Changes are displayed on the User page (#1648)

--- src/test/java/school/redrover/ManageUsersTest.java
+++ src/test/java/school/redrover/ManageUsersTest.java
@@ -1,18 +1,14 @@
 package school.redrover;
-
-import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
 public class ManageUsersTest extends BaseTest {
-
     private static final By MANAGE_JENKINS = By.xpath("//a[@href='/manage']");
     private static final By MANAGE_USERS = By.xpath("//a[@href='securityRealm/']");
     private static final By CREATE_USER = By.xpath("//a[@href = 'addUser']");
@@ -26,12 +22,8 @@ public class ManageUsersTest extends BaseTest {
     private static final By LOG_IN_USERNAME_FIELD = By.id("j_username");
     private static final By LOG_IN_PASSWORD_FIELD = By.xpath("//input[@name='j_password']");
     private static final By SUBMIT_BUTTON = By.xpath("//button[@name='Submit']");
-
     public static final String username = "Mr_Churchill";
     public static final String password = "Churchill2023";
-
-
-
     @Test
     public void testDeleteUserFromUserList() {
         createUser();
@@ -54,6 +46,30 @@ public class ManageUsersTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By
                 .xpath("//div[@class ='alert alert-danger']")).getText(),
                 "Invalid username or password");
+    }
+    @Test
+    public void testMakeChangesToUserProfile() {
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        createUser();
+        getDriver().findElement(MANAGE_JENKINS).click();
+        getDriver().findElement(MANAGE_USERS).click();
+
+        WebElement user = getDriver().findElement(By.xpath("//a[@href='user/mr_churchill/']"));
+        new Actions(getDriver()).moveToElement(user).perform();
+        WebElement chevron = getDriver().findElement(By
+                .xpath("//a[@href='user/mr_churchill/']/button"));
+        js.executeScript("arguments[0].click();", chevron);
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(By
+            .xpath("//a[@href='/user/mr_churchill/configure']"))).click();
+
+        WebElement fullNameField = getDriver().findElement(By.xpath("//input[@name='_.fullName']"));
+        fullNameField.clear();
+        fullNameField.sendKeys("Chepchik");
+        getDriver().findElement(SUBMIT_BUTTON).click();
+
+        Assert.assertEquals(getDriver().findElement(By
+                .xpath("//h1")).getText(), "Chepchik");
     }
     public void createUser() {
         getDriver().findElement(MANAGE_JENKINS).click();
