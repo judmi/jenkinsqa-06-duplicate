362ade99bc2afb836b740ee2069bc1d24ebe1523
Olesya Kolenchenko <kololesya@gmail.com>
2023-05-12 23:49:01 +0200

    remove Ignore

--- src/test/java/school/redrover/PipelineProjectTest.java
+++ src/test/java/school/redrover/PipelineProjectTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -153,7 +152,6 @@ public class PipelineProjectTest extends BaseTest {
         Assert.assertEquals(actualErrorMessage.getText(), errorMessage);
     }
 
-    @Ignore
     @Test
     public void testDiscardOldBuildsIsChecked0Builds() {
         final String builds = "0";
