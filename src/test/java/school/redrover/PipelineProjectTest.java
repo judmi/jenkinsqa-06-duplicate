02b6795dc4a08f438428725eac7f66332134977d
Olesya Kolenchenko <kololesya@gmail.com>
2023-05-12 13:25:14 +0200

    add Ignore() to testDiscardOldBuildsIsChecked0Builds()

--- src/test/java/school/redrover/PipelineProjectTest.java
+++ src/test/java/school/redrover/PipelineProjectTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -152,6 +153,7 @@ public class PipelineProjectTest extends BaseTest {
         Assert.assertEquals(actualErrorMessage.getText(), errorMessage);
     }
 
+    @Ignore
     @Test
     public void testDiscardOldBuildsIsChecked0Builds() {
         final String builds = "0";
