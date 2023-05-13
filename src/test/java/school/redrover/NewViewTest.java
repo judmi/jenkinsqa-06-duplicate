ef312fce739a3d4f8fe04a63bc8c541d16e991d5
ViktoriiaKarpus <103855583+ViktoriiaKarpus@users.noreply.github.com>
2023-05-13 18:52:52 +0200

    RF test and method (#1775)

--- src/test/java/school/redrover/NewViewTest.java
+++ src/test/java/school/redrover/NewViewTest.java
@@ -22,7 +22,7 @@ public class NewViewTest extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='/me/my-views']")).click();
         getDriver().findElement(By.xpath("//a[contains(@href, '/view/all/newJob')]")).click();
         getDriver().findElement(By.id("name")).sendKeys(NEW_VIEW_NAME_RANDOM);
-        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hudson_model_FreeStyleProject"))).click();
         getDriver().findElement(By.cssSelector("#ok-button")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
         getDriver().findElement(GO_TO_DASHBOARD_BUTTON).click();
@@ -39,7 +39,7 @@ public class NewViewTest extends BaseTest {
 
     @Test
     public void testCreateNewView() {
-        this.createNewProjectFromMyViewsPage();
+        createNewProjectFromMyViewsPage();
         getDriver().findElement(By.className("addTab")).click();
         getDriver().findElement(By.id("name")).sendKeys(NEW_VIEW_NAME_RANDOM);
         getDriver().findElement(By.xpath("//label[@for='hudson.model.ListView']")).click();
@@ -52,7 +52,7 @@ public class NewViewTest extends BaseTest {
 
     @Test
     public void testCreateNewViewSecond() {
-        this.createNewProjectFromMyViewsPage();
+        createNewProjectFromMyViewsPage();
 
         getDriver().findElement(By.cssSelector("a.addTab")).click();
         getDriver().findElement(By.cssSelector("input#name")).sendKeys(NEW_VIEW_NAME_RANDOM);
@@ -64,24 +64,24 @@ public class NewViewTest extends BaseTest {
 
     @Test
     public void testRenameView() {
-        this.createNewProjectFromMyViewsPage();
+        createNewProjectFromMyViewsPage();
         getDriver().findElement(By.className("addTab")).click();
-        getDriver().findElement(By.id("name")).sendKeys("MyFirstView");
+        getDriver().findElement(By.id("name")).sendKeys(NEW_VIEW_NAME_RANDOM);
         getDriver().findElement(By.xpath("//label[@for='hudson.model.ListView']")).click();
         getDriver().findElement(By.id("ok")).click();
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@name='Submit']")));
-        getDriver().findElement(By.linkText("Dashboard")).click();
-        getDriver().findElement(By.linkText("MyFirstView")).click();
-        getDriver()
-                .findElement(By.xpath("//div[@id='tasks']/div[@class='task ']/span[@class='task-link-wrapper ']/a[@href='/view/MyFirstView/configure']")).click();
+        getDriver().findElement(GO_TO_DASHBOARD_BUTTON).click();
+        getDriver().findElement(CREATED_LIST_VIEW).click();
+        getDriver().findElement(By.linkText("Edit View")).click();
         getDriver()
                 .findElement(By.xpath("//div[@class='setting-main']/input[@name='name']")).clear();
         getDriver()
-                .findElement(By.xpath("//div[@class='setting-main']/input[@name='name']")).sendKeys("MySecondView");
+                .findElement(By.xpath("//div[@class='setting-main']/input[@name='name']")).sendKeys("RenameView");
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.linkText("MySecondView")).getText(), "MySecondView");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='/view/RenameView/']")).getText(), "RenameView");
     }
+
     @Test
     public void testDeleteView() {
         this.createNewProjectFromMyViewsPage();
