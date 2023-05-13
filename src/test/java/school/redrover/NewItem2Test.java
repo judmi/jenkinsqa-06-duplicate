64ffd510fe673d59900ed237873678ab5e121b60
Victoria_Lem <115832814+VictoriaLema@users.noreply.github.com>
2023-05-13 04:02:03 +0300

    AT 00.00 New Item > Create all types items using a DataProvider (#1752)

--- src/test/java/school/redrover/NewItem2Test.java
+++ src/test/java/school/redrover/NewItem2Test.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -98,5 +99,35 @@ public class NewItem2Test extends BaseTest {
 
         String actualFolderName = titleName.getText();
         Assert.assertEquals(actualFolderName, expectedFolderName);
+    }
+
+    @DataProvider(name = "all-jobs-creation")
+    public Object[][] provideNamesAndTypesOfJobs() {
+        return new Object[][]{
+                {"Freestyle_project","hudson_model_FreeStyleProject"},
+                {"Pipeline", "org_jenkinsci_plugins_workflow_job_WorkflowJob"},
+                {"Multiconfiguration-project","hudson_matrix_MatrixProject"},
+                {"Folder", "com_cloudbees_hudson_plugins_folder_Folder"},
+                {"Multibranch-Pipeline", "org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject"},
+                {"Organization-Folder", "jenkins_branch_OrganizationFolder"}
+        };
+    }
+
+    @Test(dataProvider = "all-jobs-creation")
+    public void testAllJobsCreation (String name, String jobType) {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated((By.id("name")))).sendKeys(name);
+
+        getDriver().findElement(By.className(jobType)).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
+
+        getWait2().until(ExpectedConditions.textToBePresentInElement(getDriver().findElement(By.tagName("h1")),name));
+
+        getDriver().findElement(By.xpath("//a[@href='/'][@class='model-link']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']/span")).getText(), name);
     }
 }
