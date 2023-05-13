1e896943fde3ddf5b816d277baeb6d6540767599
Mariia Rykova <129385242+RykovaMariia@users.noreply.github.com>
2023-05-13 15:43:55 +0400

    AT 04.02| Rename created Folder (#1769)
    AT 04.02| Rename created Folder

--- src/test/java/school/redrover/Folder4Test.java
+++ src/test/java/school/redrover/Folder4Test.java
@@ -15,7 +15,7 @@ public class Folder4Test extends BaseTest {
     final String NAME_VIEW = "Test2";
 
     @Test
-    public void testCreateFolder() throws InterruptedException {
+    public void testCreateFolder(){
         getDriver().findElement(By.linkText("New Item")).click();
         getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='name']")))
                 .sendKeys(NAME_FOLDER);
@@ -45,12 +45,37 @@ public class Folder4Test extends BaseTest {
         WebElement myView = getDriver().findElement(By.xpath("//fieldset/div[last()]/input"));
         js.executeScript("arguments[0].scrollIntoView();", myView);
         actions.moveToElement(myView).click(myView).build().perform();
-
         getDriver().findElement(By.id("ok")).click();
-
         getDriver().findElement(By.linkText("All")).click();
-
         WebElement newView = getDriver().findElement(By.linkText(NAME_FOLDER));
+
         Assert.assertTrue(newView.isDisplayed(),"error was not shown created view");
+    }
+
+    @Test(dependsOnMethods = {"testCreateFolder"})
+    public void testRenameFolder(){
+        final String newName = "newTestName";
+        Actions actions = new Actions(getDriver());
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+
+        WebElement nameFolder = getDriver().findElement(By.cssSelector("a[href='job/" + NAME_FOLDER + "/']>span"));
+        actions.moveToElement(nameFolder).build().perform();
+        WebElement arrow = getDriver().findElement(By.cssSelector("a[href='job/" + NAME_FOLDER + "/']>button"));
+        js.executeScript("arguments[0].click();", arrow);
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[@href='/job/" + NAME_FOLDER + "/confirm-rename']"))).click();
+
+        WebElement inputFieldNewName = getDriver().findElement(By.xpath("//input[@name='newName']"));
+        inputFieldNewName.clear();
+        inputFieldNewName.sendKeys(newName);
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        getDriver().findElement(By.linkText("Dashboard")).click();
+        WebElement nameRenamedFolder = getDriver().findElement(By.cssSelector("a[href='job/" + newName + "/']>span"));
+        actions.moveToElement(nameRenamedFolder).click(nameRenamedFolder).build().perform();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("#main-panel>h1")).getText(), newName);
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("svg[title='Folder']")).isDisplayed(),
+                "error was not shown icon folder");
     }
 }
