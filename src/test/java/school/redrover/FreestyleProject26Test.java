619c56711eb47495db1e7c1e9d336d1512e4ad9c
AnnaW <66891449+MalykhAV@users.noreply.github.com>
2023-05-12 17:04:06 -0700

    AT 00.01 | Add test to create Freestyle Project (#1683)
    * AT 00.01 | New Item > Freestyle Project > Add test to create Freestyle Project
    
    * AT 00.01 | New Item > Freestyle Project > Renaming Freestyle Project class
    
    * AT 00.01 | New Item > Freestyle Project > Changing the quote configuration in xpathes
    
    * AT 00.01 | New Item > Freestyle Project > Changing the quote configuration in xpathes and the class name

---
+++ src/test/java/school/redrover/FreestyleProject26Test.java
@@ -0,0 +1,31 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreestyleProject26Test extends BaseTest {
+    private static final String NAME_FREESTYLE_PROJECT = "freestyle";
+
+    @Test
+    public void testCreate() {
+
+        WebElement newItemSideBar = getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a"));
+        newItemSideBar.click();
+
+        WebElement nameField = getDriver().findElement(By.xpath("//*[@id='name']"));
+        nameField.sendKeys(NAME_FREESTYLE_PROJECT);
+
+        WebElement freestyleProjectElement = getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]"));
+        freestyleProjectElement.click();
+
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement h1Element = getDriver().findElement(By.xpath("//*[@id='main-panel']/h1"));
+        Assert.assertEquals(h1Element.getText(), "Project " + NAME_FREESTYLE_PROJECT);
+    }
+}
