2ed4cedba67a6f96bd4316b69a5e17490b843123
AnastasiyaAbramova <128148850+AnastasiyaAbramova@users.noreply.github.com>
2023-05-13 21:34:50 +0500

    AT 03.02 | Item > Multi-Configuration >Disable project on the project page (#1778)
    * RF Multi-Configuration project > add description
    
    * RF Multi-Configuration project > add description
    
    * AT 03.02 | Item > Multi-Configuration >Disable project on the project page

--- src/test/java/school/redrover/MultiConfigurationProject1Test.java
+++ src/test/java/school/redrover/MultiConfigurationProject1Test.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -12,37 +13,45 @@ public class MultiConfigurationProject1Test extends BaseTest {
     @Test
     public void testCreateMultiConfiguration() {
 
-        String expectedResult = "test";
+        final String projectName = "test";
 
         getDriver().findElement(By.xpath("//span[text()='New Item']/../..")).click();
-        WebElement itemName = getDriver().findElement(By.xpath("//input[@name = 'name']"));
-        itemName.click();
-        itemName.sendKeys("test");
-
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name = 'name']"))).sendKeys(projectName);
         getDriver().findElement(By.xpath("//span[text()='Multi-configuration project']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-
         getDriver().findElement(By.xpath("//button[@formNoValidate='formNoValidate']")).click();
         getDriver().findElement(By.cssSelector("#breadcrumbBar > ol > li")).click();
 
         String actualResult = getDriver().findElement(By.cssSelector(" [href='job/test/']")).getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult, projectName);
     }
-
     @Test(dependsOnMethods = {"testCreateMultiConfiguration"})
 
     public void testAddDescription() {
 
         String expectedResult = "There is the test project";
+
         WebElement testButton = getDriver().findElement(By.cssSelector(" [href='job/test/']"));
-        new Actions(getDriver()).moveToElement(testButton).click().build().perform();
+        new Actions(getDriver()).moveToElement(testButton).click(testButton).build().perform();
         getDriver().findElement(By.xpath("//a[@id = 'description-link']")).click();
-        getDriver().findElement(By.xpath("//textarea")).sendKeys(expectedResult);
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//textarea"))).sendKeys(expectedResult);
         getDriver().findElement(By.xpath("(//button[@formnovalidate = 'formNoValidate'])[1]")).click();
 
-        String actualResult = getDriver().findElement(By.cssSelector("#description>div:first-child ")).getText();
+        String actualResult = getDriver().findElement(By.xpath("//div[text() = 'There is the test project'] ")).getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+    @Test(dependsOnMethods = {"testCreateMultiConfiguration"})
+    public void testDisableProject() {
+
+        String expectedResult = "This project is currently disabled";
+
+        WebElement testButton = getDriver().findElement(By.cssSelector(" [href='job/test/']"));
+        new Actions(getDriver()).moveToElement(testButton).click(testButton).build().perform();
+        getDriver().findElement(By.xpath("//form/button[@formnovalidate = 'formNoValidate']")).click();
+        String actualResult = getDriver().findElement(By.xpath("//form[@id = 'enable-project']")).getText();
+
+        Assert.assertTrue(actualResult.contains(expectedResult));
     }
 }
