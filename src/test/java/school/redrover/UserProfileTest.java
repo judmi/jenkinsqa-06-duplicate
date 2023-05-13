67a32f448261d5cbc55ead8fb4301502230c363f
Vera Fes <verafes@gmail.com>
2023-05-12 19:55:05 -0700

    AT 09.05 | Dashboard > Change the user profile > Update description for User (#1761)
    * AT 09.05 | Dashboard > Change the user profile > Update description for User
    
    * fix test

--- src/test/java/school/redrover/UserProfileTest.java
+++ src/test/java/school/redrover/UserProfileTest.java
@@ -14,7 +14,7 @@ public class UserProfileTest extends BaseTest {
     protected static final String email = "test@test.com";
     protected static final String fullName = "Test User";
 
-    public void createUser() {
+    public void createUser(String username, String password, String fullName, String email)  {
         getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
         getDriver().findElement(By.xpath("//a[@href='securityRealm/']")).click();
         getDriver().findElement(By.xpath("//a[@href='addUser']")).click();
@@ -30,7 +30,8 @@ public class UserProfileTest extends BaseTest {
     public void testAddDescriptionToUser() {
         final String displayedDescriptionText = "Test User Description";
 
-        createUser();
+        createUser(username, password, fullName, email);
+
         getDriver().findElement(By.xpath("//a[contains(@href, '" + username + "')]")).click();
         getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
 
@@ -45,5 +46,30 @@ public class UserProfileTest extends BaseTest {
                 .findElement(By.xpath("//div[@id='description']/div[1]")).getText();
 
         Assert.assertEquals(actualDisplayedDescriptionText, displayedDescriptionText);
+    }
+
+    @Test(dependsOnMethods = {"testAddDescriptionToUser"})
+    public void testUpdateDescriptionToUser() {
+        final String displayedDescriptionText = "User Description Updated";
+        final String existingDescriptionText = "Test User Description";
+
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+        getDriver().findElement(By.xpath("//a[@href='securityRealm/']")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href, '" + username + "')]")).click();
+
+        getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
+
+        WebElement descriptionInputField = getDriver()
+                .findElement(By.xpath("//textarea[@name='description']"));
+        descriptionInputField.click();
+        descriptionInputField.clear();
+        descriptionInputField.sendKeys(displayedDescriptionText);
+        getDriver().findElement(By.name("Submit")).click();
+
+        String actualDisplayedDescriptionText = getDriver()
+                .findElement(By.xpath("//div[@id='description']/div[1]")).getText();
+
+        Assert.assertEquals(actualDisplayedDescriptionText, displayedDescriptionText);
+        Assert.assertNotEquals(actualDisplayedDescriptionText, existingDescriptionText);
     }
 }
