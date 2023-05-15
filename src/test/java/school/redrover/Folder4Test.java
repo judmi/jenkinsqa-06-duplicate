fc6a0f65e843bf5cc141ae556dca665413b091f7
Mariia Rykova <129385242+RykovaMariia@users.noreply.github.com>
2023-05-16 01:20:17 +0400

    AT 04.03 | Folder > Move Folder to Folder (#1861)

--- src/test/java/school/redrover/Folder4Test.java
+++ src/test/java/school/redrover/Folder4Test.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -16,8 +17,20 @@ public class Folder4Test extends BaseTest {
     final String NAME_FOLDER = "Test";
     final String NAME_VIEW = "Test View";
 
+    private void projectDropDownMenu(String nameProject, String nameItemMenu) {
+        Actions actions = new Actions(getDriver());
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+
+        WebElement nameFolder = getDriver().findElement(By.xpath("//span[contains(text(),'" + nameProject + "')]"));
+        actions.moveToElement(nameFolder).perform();
+        WebElement arrow = getDriver().findElement(By.cssSelector("a[href='job/" + nameProject + "/']>button"));
+        js.executeScript("arguments[0].click();", arrow);
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[contains(text(), '" + nameItemMenu + "')]"))).click();
+    }
+
     @Test
-    public void testCreateFolder(){
+    public void testCreateFolder() {
         TestUtils.createFolder(this, NAME_FOLDER, true);
 
         WebElement nameFolder = getDriver().findElement(By.xpath("//span[contains(text(),'" + NAME_FOLDER + "')]"));
@@ -29,7 +42,7 @@ public class Folder4Test extends BaseTest {
     }
 
     @Test(dependsOnMethods = {"testCreateFolder"})
-    public void testCreateNewViewInFolder(){
+    public void testCreateNewViewInFolder() {
         Actions actions = new Actions(getDriver());
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
 
@@ -49,17 +62,9 @@ public class Folder4Test extends BaseTest {
     }
 
     @Test(dependsOnMethods = {"testCreateFolder"})
-    public void testRenameFolder(){
+    public void testRenameFolder() {
         final String newName = "newTestName";
-        Actions actions = new Actions(getDriver());
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-
-        WebElement nameFolder = getDriver().findElement(By.xpath("//span[contains(text(),'" + NAME_FOLDER + "')]"));
-        actions.moveToElement(nameFolder).perform();
-        WebElement arrow = getDriver().findElement(By.cssSelector("a[href='job/" + NAME_FOLDER + "/']>button"));
-        js.executeScript("arguments[0].click();", arrow);
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[@href='/job/" + NAME_FOLDER + "/confirm-rename']"))).click();
+        projectDropDownMenu(NAME_FOLDER, "Rename");
 
         WebElement inputFieldNewName = getDriver().findElement(By.xpath("//input[@name='newName']"));
         inputFieldNewName.clear();
@@ -68,10 +73,27 @@ public class Folder4Test extends BaseTest {
 
         getDriver().findElement(By.linkText("Dashboard")).click();
         WebElement nameRenamedFolder = getDriver().findElement(By.cssSelector("a[href='job/" + newName + "/']>span"));
-        actions.moveToElement(nameRenamedFolder).click(nameRenamedFolder).perform();
+        new Actions(getDriver()).moveToElement(nameRenamedFolder).click(nameRenamedFolder).perform();
 
         Assert.assertEquals(getDriver().findElement(By.cssSelector("#main-panel>h1")).getText(), newName);
         Assert.assertTrue(getDriver().findElement(By.cssSelector("svg[title='Folder']")).isDisplayed(),
                 "error was not shown icon folder");
+    }
+
+    @Test
+    public void testMoveFolderToFolder(){
+        TestUtils.createFolder(this, NAME_FOLDER, true);
+        TestUtils.createFolder(this, NAME_FOLDER + " 2", true);
+
+        projectDropDownMenu(NAME_FOLDER, "Move");
+
+        WebElement selectInput = getDriver().findElement(By.xpath("//select"));
+        new Select(selectInput).selectByVisibleText("Jenkins » Test 2");
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[contains(text(),'" + NAME_FOLDER + " 2" + "')]")).click();
+
+        Assert.assertTrue(
+                getDriver().findElement(By.xpath("//a[contains(text(),'" + NAME_FOLDER + "')]")).isDisplayed(),
+                "error was not shown moved folder");
     }
 }
