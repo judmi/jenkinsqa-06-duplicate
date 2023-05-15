2f069d9737e6dcbd9a7f25c2cd7678f383460ca8
lyudmila171 <lyudmila17@gmail.com>
2023-05-15 13:14:02 -0500

    RF 04.15 | Folder > Move Multibranch Pipeline to Folder change methods for Folder and Multibranch Pipeline creation

--- src/test/java/school/redrover/MultibranchPipeline3Test.java
+++ src/test/java/school/redrover/MultibranchPipeline3Test.java
@@ -12,66 +12,23 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import org.openqa.selenium.support.ui.Select;
+import school.redrover.runner.TestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MultibranchPipeline3Test extends BaseTest {
-    private void createFolder() {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Folder1");
-        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
-        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-    }
 
-    private void createMultibranchPipeline() {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("MultibranchPipeline1");
-        getDriver().findElement(By.xpath("//span[text()='Multibranch Pipeline']")).click();
-        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-    }
-
-    @Ignore
     @Test
     public void testMoveMultibranchPipelineToFolder() {
-        createFolder();
-        getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']/ol[@id='breadcrumbs']/li[@class='jenkins-breadcrumbs__list-item']/a[text()='Dashboard']")).click();
-        createMultibranchPipeline();
+        TestUtils.createFolder(this,"Folder1",true);
+        TestUtils.createMultibranchPipeline(this,"MultibranchPipeline1", false);
         getDriver().findElement(By.linkText("Move")).click();
         WebElement folderList = getDriver().findElement(By.xpath("//select[@name='destination']"));
         getWait2().until(ExpectedConditions.elementToBeClickable(folderList)).click();
         getDriver().findElement(By.xpath("//select/option[contains(text(),'Folder')]")).click();
         getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
 
-        String expectedMultibranchPipeline = "MultibranchPipeline1";
-        WebElement actualMultibranchPipeline = getDriver().findElement(By.xpath("//h1"));
-        Assert.assertEquals(actualMultibranchPipeline.getText(), expectedMultibranchPipeline);
-
-        new Actions(getDriver())
-                .click(getDriver().findElement(By.linkText("Folder1")))
-                .perform();
-        WebElement actualMultibranchPipeline2 = getDriver().findElement(By.linkText("MultibranchPipeline1"));
-
-        Assert.assertEquals(actualMultibranchPipeline2.getText(), expectedMultibranchPipeline);
-    }
-
-    @Test
-    public void testMoveMultibranchPipelineToFolderByDrop() {
-        createFolder();
-        getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']/ol[@id='breadcrumbs']/li[@class='jenkins-breadcrumbs__list-item']/a[text()='Dashboard']")).click();
-        createMultibranchPipeline();
-        getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']/ol[@id='breadcrumbs']/li[@class='jenkins-breadcrumbs__list-item']/a[text()='Dashboard']")).click();
-        new Actions(getDriver())
-                .moveToElement(getDriver().findElement(By.xpath("//span[text() = 'MultibranchPipeline1']")))
-                .perform();
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='job/MultibranchPipeline1/']/button[@class = 'jenkins-menu-dropdown-chevron']"))).click();
-        getDriver().findElement(By.xpath("//a[@href='/job/MultibranchPipeline1/move']")).click();
-        WebElement folderList = getDriver().findElement(By.xpath("//select[@name='destination']"));
-        getWait2().until(ExpectedConditions.elementToBeClickable(folderList)).click();
-        getDriver().findElement(By.xpath("//select/option[contains(text(),'Folder')]")).click();
-        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
         String expectedMultibranchPipeline = "MultibranchPipeline1";
         WebElement actualMultibranchPipeline = getDriver().findElement(By.xpath("//h1"));
         Assert.assertEquals(actualMultibranchPipeline.getText(), expectedMultibranchPipeline);
