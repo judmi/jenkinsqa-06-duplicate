5c6c127c7893c09b08d3f05287b835e78fd5a366
AnnaAbg <106620445+AnnaAbg@users.noreply.github.com>
2023-05-15 19:26:56 +0300

    AT 01.01 | Freestyle project_View project page_Disable the project (#1828)
    * AT 01.01 | Freestyle project_View project page_Disable the project
    
    * Fix the locator

--- src/test/java/school/redrover/CreateFreestyleProject2Test.java
+++ src/test/java/school/redrover/CreateFreestyleProject2Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -15,7 +16,7 @@ public class CreateFreestyleProject2Test extends BaseTest {
 
         getDriver().findElement(By.xpath("//span[text()='New Item']/../..")).click();
 
-        WebElement itemName = getDriver().findElement(By.xpath("//input[@name = 'name']"));
+        WebElement itemName = getDriver().findElement(By.id("name"));
         itemName.click();
         itemName.sendKeys("NewProject");
         getDriver().findElement(By.xpath("//span [@class = 'label']")).click();
@@ -29,5 +30,23 @@ public class CreateFreestyleProject2Test extends BaseTest {
                 .getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(dependsOnMethods = "testCreateNewFreestyleProject")
+    public void testDisableFreestyleProject() {
+        getDriver().findElement(By.xpath("//a [@href = 'job/NewProject/']/span")).click();
+
+        WebElement disableProject = getWait5().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(
+                By.xpath("//button[@name='Submit']"))));
+        disableProject.click();
+
+        WebElement actualErrorMessageWarning = getDriver().findElement(By.id("enable-project"));
+        Assert.assertEquals(actualErrorMessageWarning.getText().substring(0, 34), "This project is currently disabled");
+
+        getDriver().findElement(By.xpath("//*[@id='jenkins-head-icon']")).click();
+
+        WebElement iconDisabled = getDriver().findElement(By.xpath("//*[@tooltip='Disabled']"));
+
+        Assert.assertTrue(iconDisabled.isDisplayed());
     }
 }
