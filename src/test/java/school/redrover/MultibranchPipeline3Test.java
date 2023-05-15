fc1eefc0faefdebda0a833c993f0019b15b1b36b
lyudmila171 <lyudmila17@gmail.com>
2023-05-15 13:24:17 -0500

    AT 04.15 | Folder > Move Multibranch Pipeline to Folder with dropdown menu

--- src/test/java/school/redrover/MultibranchPipeline3Test.java
+++ src/test/java/school/redrover/MultibranchPipeline3Test.java
@@ -40,4 +40,28 @@ public class MultibranchPipeline3Test extends BaseTest {
 
         Assert.assertEquals(actualMultibranchPipeline2.getText(), expectedMultibranchPipeline);
     }
+    @Test
+    public void testMoveMultibranchPipelineToFolderByDrop() {
+        TestUtils.createFolder(this,"Folder1",true);
+        TestUtils.createMultibranchPipeline(this,"MultibranchPipeline1", true);
+        new Actions(getDriver())
+                .moveToElement(getDriver().findElement(By.xpath("//span[text() = 'MultibranchPipeline1']")))
+                .perform();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='job/MultibranchPipeline1/']/button[@class = 'jenkins-menu-dropdown-chevron']"))).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/MultibranchPipeline1/move']")).click();
+        WebElement folderList = getDriver().findElement(By.xpath("//select[@name='destination']"));
+        getWait2().until(ExpectedConditions.elementToBeClickable(folderList)).click();
+        getDriver().findElement(By.xpath("//select/option[contains(text(),'Folder')]")).click();
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
+        String expectedMultibranchPipeline = "MultibranchPipeline1";
+        WebElement actualMultibranchPipeline = getDriver().findElement(By.xpath("//h1"));
+        Assert.assertEquals(actualMultibranchPipeline.getText(), expectedMultibranchPipeline);
+
+        new Actions(getDriver())
+                .click(getDriver().findElement(By.linkText("Folder1")))
+                .perform();
+        WebElement actualMultibranchPipeline2 = getDriver().findElement(By.linkText("MultibranchPipeline1"));
+
+        Assert.assertEquals(actualMultibranchPipeline2.getText(), expectedMultibranchPipeline);
+    }
 }
