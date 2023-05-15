bc4cda0dc37f09a5b52bfbe6cfbebe136b627e85
msnata <msnata@ukr.net>
2023-05-15 19:45:46 +0300

    testDisableFreestyleProject_created (#1841)

--- src/test/java/school/redrover/FreestyleProject5Test.java
+++ src/test/java/school/redrover/FreestyleProject5Test.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
 public class FreestyleProject5Test extends BaseTest {
 
@@ -23,5 +24,18 @@ public class FreestyleProject5Test extends BaseTest {
                 getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']/span"));
 
         Assert.assertEquals(projectName.getText(), "First Freestyle Project");
+    }
+
+    @Test
+    public void testDisableFreestyleProject(){
+        TestUtils.createFreestyleProject(this, "First Freestyle Project");
+
+        getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']/span")).click();
+        getDriver().findElement(By.xpath("//form[@id='disable-project']/button")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//form[@id='enable-project']"))
+                .getText().trim().substring(0, 34),"This project is currently disabled" );
+
+        getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']//a[@class='model-link']")).click();
     }
 }
