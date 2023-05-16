f331231a837514218a6fc61caf3b31b6b41f3fde
Rumeantev Veaceslav <slava2045@gmail.com>
2023-05-17 00:38:44 +0300

    AT 02.07 | Pipeline > Rename from Pipeline page (#1903)

--- src/test/java/school/redrover/PipelineProject4Test.java
+++ src/test/java/school/redrover/PipelineProject4Test.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class PipelineProject4Test extends BaseTest {
+    private static final By JENKINS_ICON = By.xpath("//img[@id='jenkins-name-icon']");
 
     @Test
     public void testCreateNewPipelineProject() {
@@ -20,6 +21,23 @@ public class PipelineProject4Test extends BaseTest {
 
         WebElement pipelineName = getDriver().findElement(By.xpath("//h1[normalize-space()='Pipeline Test']"));
         Assert.assertEquals(pipelineName.getText(), "Pipeline " + testData );
+        getDriver().findElement(JENKINS_ICON).click();
+    }
 
+    @Test
+    public void testRenameFromPipelinePage() {
+        final String newName = "First Test";
+        testCreateNewPipelineProject();
+
+        getDriver().findElement(By.xpath("//span[normalize-space()='Test']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/Test/confirm-rename']")).click();
+        WebElement nameField = getDriver().findElement(By.xpath("//input[@name='newName']"));
+        nameField.clear();
+        nameField.sendKeys(newName);
+        getDriver().findElement(By.xpath("//button[normalize-space()='Rename']")).click();
+        getDriver().findElement(JENKINS_ICON).click();
+
+        WebElement jobName =  getDriver().findElement(By.xpath("//span[normalize-space()='First Test']"));
+        Assert.assertEquals(jobName.getText(), newName);
     }
 }
