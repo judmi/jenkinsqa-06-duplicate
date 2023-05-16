3a11bec820a0444c75303ef76ccbba5c8d32c1ce
UlyanaVer <uveryaskina@gmail.com>
2023-05-16 22:33:11 +0300

    At 00.02 pipeline project incorrect symbol (#1906)
    * add my test logout2
    
    * Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Вторая попытка отправить:
    Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Два теста на Pipeline create
    TC 00.02 | New Item > Create Pipeline Project
    
    * Maven проблемма
    TC 00.02 | New Item > Create Pipeline Project
    
    * RF CreatePipelineProject5Test > past
    
    * RF 00.02 | CreatePipelineProject7Test > addName
    
    * AT 00.02| Pipeline Project > wrong characters Before the name project
    https://trello.com/c/nRsass8U

--- src/test/java/school/redrover/PipelineProject7Test.java
+++ src/test/java/school/redrover/PipelineProject7Test.java
@@ -1,13 +1,16 @@
 package school.redrover;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
 public class PipelineProject7Test extends BaseTest {
-        private String name1 = "Project1";
+    private String name1 = "Project1";
+
     @Test
     public void testCreatePipelineProject() {
         TestUtils.createPipeline(this, name1, true);
@@ -16,5 +19,35 @@ public class PipelineProject7Test extends BaseTest {
                 .findElement(By.xpath("//tr[@id='job_" + name1 + "']//a//span['" + name1 + "']"))
                 .getText(), name1);
     }
-}
 
+    @DataProvider(name = "wrong-characters")
+    public Object[][] providerWrongCharacters() {
+        return new Object[][]{{"!"}, {"@"}, {"#"}, {"$"}, {"%"}, {"^"}, {"&"}, {"*"}, {"?"}, {"|"}, {">"}, {"["}, {"]"}};
+
+    }
+
+    @Test(dataProvider = "wrong-characters")
+    public void testWrongCharactersBeforeNameProject(String wrongCharacters) {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("name"))).sendKeys(wrongCharacters);
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.id("itemname-invalid"))
+                .getText(), "» ‘" + wrongCharacters + "’ is an unsafe character");
+
+        Assert.assertFalse(getDriver().findElement(By.id("ok-button")).isEnabled());
+
+        getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
+    }
+
+    @Test
+    public void testDotBeforeNameProject() {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("name"))).sendKeys(".");
+
+        Assert.assertEquals(getDriver().findElement(By.id("itemname-invalid"))
+                .getText(), "» “.” is not an allowed name");
+    }
+}
