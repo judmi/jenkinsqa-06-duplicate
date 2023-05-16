fb5cf6e850c6249c35f50546bb482f12d6ca4447
Irina Sh <104162505+Kononirin@users.noreply.github.com>
2023-05-16 09:11:52 +0500

    Add testRenameFolder (#1846)

--- src/test/java/school/redrover/Folder5Test.java
+++ src/test/java/school/redrover/Folder5Test.java
@@ -1,7 +1,11 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -22,5 +26,24 @@ public class Folder5Test extends BaseTest {
         WebElement folderName = getDriver().findElement(By.xpath("//span[text() = 'New folder']"));
 
         Assert.assertEquals(folderName.getText(), "New folder");
+    }
+
+    @Test(dependsOnMethods = "testCreateNewFolder")
+    public void testRenameFolder() {
+        WebElement dropdown = getDriver().findElement(By.xpath("//a[@href='job/New%20folder/']/button"));
+        new Actions(getDriver()).moveToElement(dropdown).perform();
+        dropdown.sendKeys(Keys.RETURN);
+        getWait2().until(ExpectedConditions.elementToBeClickable(By
+                .xpath("//a[@href='/job/New%20folder/confirm-rename']"))).click();
+
+        WebElement input = getDriver().findElement(By.xpath("//input[@name='newName']"));
+        input.clear();
+        input.sendKeys("New name folder");
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        getDriver().findElement(DASHBOARD).click();
+        WebElement newFolderName = getDriver().findElement(By.xpath("//a[@href='job/New%20name%20folder/']/span"));
+
+        Assert.assertEquals(newFolderName.getText(), "New name folder");
     }
 }
