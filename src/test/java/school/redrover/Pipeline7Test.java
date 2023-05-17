baa2e7694030777d92168cc5bd93d89ef8097f9f
Dima <122726476+Kridim@users.noreply.github.com>
2023-05-17 10:42:54 +0300

    AT 02.03 | Pipeline > Build now (#1874)
    * AT 00.02| New Item > Create Pipeline Project
    
    * AT 02.03 | Pipeline > Build now
    
    ---------
    
    Co-authored-by: DimaK <dimalrivenya@mail.ru>

--- src/test/java/school/redrover/Pipeline7Test.java
+++ src/test/java/school/redrover/Pipeline7Test.java
@@ -1,6 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -16,5 +18,15 @@ public class Pipeline7Test extends BaseTest {
         String actualRes = getDriver().findElement(By.cssSelector(".jenkins-table>:nth-child(2)>tr>:nth-child(3)>a")).getText();
 
         Assert.assertEquals(actualRes, namePipeline);
+    }
+    @Test(dependsOnMethods = {"testCreatePipelineProject"})
+    public void testBuildPipeline() {
+        new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath("//span[contains(text(),'First Pipeline')]"))).click().perform();
+        getDriver().findElement(By.cssSelector("[href*='build?']")).click();
+        getDriver().findElement(By.cssSelector("#buildHistory>div>div>span>div>:nth-child(2)")).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.cssSelector("[href$='console']"))).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("(//*[name()='svg'][@title='Success'])[1]")).isDisplayed(), "Build failed");
+        Assert.assertTrue(getDriver().findElement(By.cssSelector(".jenkins-icon-adjacent")).isDisplayed(), "Not found build");
     }
 }
