4f63633044545b01e8cb86c21b016c23457d29c1
Aleksandr Morozov <131560179+aleksandrmorozovv11@users.noreply.github.com>
2023-05-17 19:58:34 -0400

    17.05 | Page-header > Logout btn > Log out button transfer the user b… (#1920)
    * 17.05 | Page-header > Logout btn > Log out button transfer the user back to the login page 'Welcome to Jenkins!'
    
    * 17.05 | Page-header > Logout btn > Log out button transfer the user back to the login page 'Welcome to Jenkins!'
    Added missed click()

--- src/test/java/school/redrover/ProfileTest.java
+++ src/test/java/school/redrover/ProfileTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -24,6 +25,17 @@ public class ProfileTest extends BaseTest {
             ((JavascriptExecutor) getDriver()).executeScript("document.querySelector('.login .jenkins-menu-dropdown-chevron').click()");
             getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='" + entry.getKey() + "']"))).click();
             Assert.assertTrue(getDriver().getCurrentUrl().contains(entry.getValue()));
+        }
+    }
+
+    @Test
+    public void testLogout(){
+        getDriver().findElement(By.xpath("//span[text()='log out']")).click();
+        try {
+            getDriver().findElement(By.xpath("//h1[text()='Welcome to Jenkins!']"));
+            Assert.assertTrue(true);
+        } catch (NoSuchElementException e) {
+            Assert.fail("Logout did not work as expected or the expected text is not present on the page");
         }
     }
 }
