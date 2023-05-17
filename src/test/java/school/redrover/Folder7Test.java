a4a6aa206fe310ae97f83ed1c969f198324a0075
NadiaSergeeva <130238321+NadiaSergeeva@users.noreply.github.com>
2023-05-17 09:46:16 +0100

    At 04.00 folder-> create folder from dashboard dropdown menu (#1877)
    * AT 04.00 | Folder > Create Folder from Dashboard dropdown menu
    * update fillFolderNameField to use Keys.RETURN

--- src/test/java/school/redrover/Folder7Test.java
+++ src/test/java/school/redrover/Folder7Test.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,27 +14,45 @@ import java.util.List;
 
 public class Folder7Test extends BaseTest {
     private static final By NAME=By.name("name");
-    private final String BEFORERENAME = "folder1";
+    private final String FOLDERNAME = "folder1";
     private final String AFTERRENAME = "folder2";
 
-        public void fillFolderNameField (String name) {
+        public void fillFolderNameField (String name, String wayToCreate) {
             getDriver().findElement(By.id("jenkins-home-link")).click();
-            getWait2().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"))));
-            getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
 
-            WebElement nameinput=getDriver().findElement(NAME);
-            getWait5().until(ExpectedConditions.visibilityOf(nameinput));
-            nameinput.sendKeys(name);
+            if (wayToCreate.equals("leftside")) {
+                getWait2().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"))));
+                getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+            }
+            else if (wayToCreate.equals("dropdown")) {
+                getWait2().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.linkText("Dashboard"))));
+
+                WebElement dashboard = getDriver().findElement(By.linkText("Dashboard"));
+                WebElement pointer = getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']//button[@class=\"jenkins-menu-dropdown-chevron\"]"));
+                new Actions(getDriver())
+                        .moveToElement(dashboard)
+                        .perform();
+                pointer.sendKeys(Keys.RETURN);
+
+                new Actions (getDriver())
+                        .click(getDriver().findElement(By.xpath("//ul[@class='first-of-type']//span[text()='New Item']")))
+                        .perform();
+            }
+            else {
+                System.out.println("wayToCreate is incorrect. Please use leftside or dropdown");
+            }
+
+            getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(NAME))).sendKeys(name);
         }
 
-        public void createBaseFolder (String name) {
-            fillFolderNameField(name);
+        public void createFolder (String name, String wayToCreate){
+            fillFolderNameField(name, wayToCreate);
 
-            WebElement folderButton = getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']"));
+            WebElement folderTypeButton = getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']"));
             new Actions(getDriver())
-                    .scrollToElement(folderButton)
+                    .scrollToElement(folderTypeButton)
                     .perform();
-            folderButton.click();
+            folderTypeButton.click();
 
             getWait2().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.id("ok-button")))).click();
         }
@@ -51,27 +70,34 @@ public class Folder7Test extends BaseTest {
 
             getDriver().findElement(By.name("Submit")).click();
         }
-        @DataProvider(name="folderNames")
+
+        @DataProvider(name= "folderNames")
         public Object[][] provideFolderNames(){
             return new Object[][]
                     {{"folder1"}, {" Spaces "}, {"123"}};
         }
 
         @Test(dataProvider = "folderNames")
-        public void testCreateFolderPositive (String folderName){
-                createBaseFolder(folderName);
-                folderName = folderName.trim();
-                getWait2().until(ExpectedConditions.textToBe(By.xpath("//h1[text()='Configuration']"), "Configuration"));
-                getDriver().findElement(By.xpath("//a[normalize-space()='"+folderName+"']")).click();
+        public void testCreateFolderByLeftside(String folderName){
+            createFolder(folderName,"leftside" );
+            folderName = folderName.trim();
+            getWait2().until(ExpectedConditions.textToBe(By.xpath("//h1[text()='Configuration']"), "Configuration"));
+            getDriver().findElement(By.xpath("//a[normalize-space()='"+folderName+"']")).click();
 
-                Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='"+folderName+"']")).getText(),folderName);
+            Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='"+folderName+"']")).getText(),folderName);
+        }
+
+        @Test
+        public void testcreateFolderFromDropdown(){
+            createFolder(FOLDERNAME, "dropdown");
+            Assert.assertEquals(getDriver().findElement(By.linkText(FOLDERNAME)).getText(),FOLDERNAME);
         }
 
         @Test
         public void testCreateFolderNegative () {
             List<String> negativevalues = Arrays.asList(" ");
             for (String value : negativevalues) {
-                createBaseFolder(value);
+                createFolder(value,"leftside" );
                 Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Error");
             }
         }
@@ -84,7 +110,7 @@ public class Folder7Test extends BaseTest {
 
         @Test(dataProvider ="special-symbols")
         public void testCreateFolderSpecSymbols (String specvalue) {
-                fillFolderNameField(specvalue);
+                fillFolderNameField(specvalue,"leftside");
                 getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.id("itemname-invalid"))));
                 Assert.assertEquals(getDriver().findElement(By.id("itemname-invalid")).getText(), "» ‘"+specvalue+"’ is an unsafe character");
 
@@ -92,16 +118,16 @@ public class Folder7Test extends BaseTest {
                 nameinput.clear();
         }
 
-        @Test(dependsOnMethods = "testCreateFolderPositive")
+        @Test(dependsOnMethods = "testCreateFolderByLeftside")
         public void testRenameFolderPositive (){
-            renameFolder(BEFORERENAME, AFTERRENAME);
+            renameFolder(FOLDERNAME, AFTERRENAME);
             Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='"+AFTERRENAME+"']")).getText(),AFTERRENAME);
         }
 
         @Test
         public void testRenameFolderNegative (){
-            createBaseFolder(BEFORERENAME);
-            renameFolder(BEFORERENAME, BEFORERENAME);
+            createFolder(FOLDERNAME, "leftside");
+            renameFolder(FOLDERNAME, FOLDERNAME);
             Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Error");
             Assert.assertEquals(getDriver().findElement(By.cssSelector("div[id='main-panel'] p")).getText(), "The new name is the same as the current name.");
         }
