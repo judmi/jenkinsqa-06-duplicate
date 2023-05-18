f941ee9831fedf666958d9a86c441ff751e56453
UlyanaVer <uveryaskina@gmail.com>
2023-05-18 16:05:43 +0300

    At 00.01 find a created freestyle project on the dashboard page (#1989)
    * AT 00.01 | Find a created Freestyle project on the Dashboard page
    https://trello.com/c/92FNc2QH

--- src/test/java/school/redrover/FreeStyleProject13Test.java
+++ src/test/java/school/redrover/FreeStyleProject13Test.java
@@ -8,11 +8,22 @@ import school.redrover.runner.TestUtils;
 
 public class FreeStyleProject13Test extends BaseTest {
     private String name = "FreestyleProject1";
+
     @Test
-    public void testCreateFreestyleProjectNewItem(){
+    public void testCreateFreestyleProjectNewItem() {
         TestUtils.createFreestyleProject(this, name, false);
+
         Assert.assertEquals(getDriver()
                 .findElement(By.xpath("//div[@id ='main-panel']//h1[text()='Project " + name + "']"))
-                .getText(),"Project " + name);
+                .getText(), "Project " + name);
+    }
+
+    @Test
+    public void testFindFreestyleProjectOnDashboard() {
+        TestUtils.createFreestyleProject(this, name, true);
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.xpath("//tr[@id='job_" + name + "']//a//span['" + name + "']"))
+                .getText(), name);
     }
 }
