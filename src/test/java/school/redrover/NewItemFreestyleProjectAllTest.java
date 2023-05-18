03232b111b125fbfc45e105afeffaf639449228f
Lana K <nika.benz.21@gmail.com>
2023-05-18 23:42:54 +0300

    AT 00.01 | New Item>Freestyle Project>Create Freestyle Project with wildcard in the name (#2000)

--- src/test/java/school/redrover/NewItemFreestyleProjectAllTest.java
+++ src/test/java/school/redrover/NewItemFreestyleProjectAllTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -27,5 +28,24 @@ public class NewItemFreestyleProjectAllTest extends BaseTest {
                 .getText();
 
         Assert.assertEquals(actualProjectNameOnDashboard, projectName);
+    }
+
+    @DataProvider(name = "wildcards")
+    public Object[][] provideWildcardInName() {
+        return new Object[][]{{"!"}, {"@"}, {"#"}, {"$"}, {"%"}, {"^"}, {"&"}, {"*"}, {"["},
+                {"]"}, {"?"}, {"<"}, {">"}, {"/"}, {"|"}, {":"}, {";"}};
+    }
+
+    @Test(dataProvider = "wildcards")
+    public void testCreateFreestyleProjectWithInvalidData(String wildcard) {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("name"))).sendKeys(wildcard);
+
+        String actualWarningUnsafeMessage = getDriver().findElement(By.id("itemname-invalid")).getText();
+
+        getDriver().findElement(By.cssSelector(".icon-freestyle-project")).click();
+
+        Assert.assertEquals(actualWarningUnsafeMessage, "» ‘" + wildcard + "’ is an unsafe character");
+        Assert.assertFalse(getDriver().findElement(By.id("ok-button")).isEnabled(), "Button is disabled, but if you see this message, button is enabled");
     }
 }
