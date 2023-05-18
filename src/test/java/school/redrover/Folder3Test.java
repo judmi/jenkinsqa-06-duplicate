981e5090fe33099af9bc3dc8b662ef3c3727fe2f
EleonoraButonaeva <130123558+EleonoraButonaeva@users.noreply.github.com>
2023-05-18 07:40:16 +0700

    AT 04.06 | Folder > Create Freestyle Project in Folder (#1954)
    * add testAddDescription
    
    * delete testAddDescription and add testCreateUser
    
    * AT 04.06 | Folder > Create Freestyle Project in Folder
    
    * AT 04.06 | Folder > Create Freestyle Project in Folder (change xpath)
    
    * AT 04.06 | Folder > Create Freestyle Project in Folder (change xpath)
    
    * AT 04.06 | Folder > Create Freestyle Project in Folder (add changes)

--- src/test/java/school/redrover/Folder3Test.java
+++ src/test/java/school/redrover/Folder3Test.java
@@ -1,15 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Folder3Test extends BaseTest {
 
-    @Ignore
     @Test
     public void testCreateFolder() {
 
@@ -32,12 +31,30 @@ public class Folder3Test extends BaseTest {
         WebElement dashboard = getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']"));
         dashboard.click();
 
-        WebElement folderName = getDriver().findElement(By.xpath("//span[normalize-space()='folder']"));
-        folderName.click();
-        folderName.click();
+        WebElement folderName = getDriver().findElement(By.xpath("(//span[normalize-space()='folder'])[1]"));
 
-        WebElement textFolder = getDriver().findElement(By.xpath("//h1[normalize-space()='folder']"));
+        Assert.assertEquals(folderName.getText(), "folder");
+    }
 
-        Assert.assertEquals(textFolder.getText(), "folder");
+    @Test(dependsOnMethods = "testCreateFolder")
+    public void testCreateFreestyleProjectInFolder() {
+
+        getDriver().findElement(By.xpath("//span[normalize-space()='folder']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='/job/folder/newJob']")).click();
+
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("new project");
+
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[normalize-space()='Save']")).click();
+
+        getDriver().findElement(By.xpath("//a[normalize-space()='folder']")).sendKeys(Keys.RETURN);
+
+        WebElement projectName = getDriver().findElement(By.xpath("//span[normalize-space()='new project']"));
+
+        Assert.assertEquals(projectName.getText(), "new project");
     }
 }
