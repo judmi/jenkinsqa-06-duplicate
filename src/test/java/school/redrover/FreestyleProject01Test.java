168545785d7e0393265221abbd8277beb992c136
alexpyatachenko <59926373+alexpyatachenko@users.noreply.github.com>
2023-05-19 11:33:27 +0300

    AT 01.01 | New Item > Freestyle Project > Create Freestyle Project (#2020)
    Add testNameAndDescAreDisplayed + little REF(replace name of the freestyle project to const FREESTYLE_NAME)
    https://trello.com/c/Qa42FTBW/1289-us-0101-freestyle-project-view-project-page

--- src/test/java/school/redrover/FreestyleProject01Test.java
+++ src/test/java/school/redrover/FreestyleProject01Test.java
@@ -7,18 +7,34 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FreestyleProject01Test extends BaseTest {
+    private static final String FREESTYLE_NAME = "Freestyle";
 
     @Test
     public void testCreateFreestyleProject(){
-        String name = "Freestyle";
         getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
 
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("name"))).sendKeys(name);
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("name"))).sendKeys(FREESTYLE_NAME);
         getDriver().findElement(By.xpath("//li[@class = 'hudson_model_FreeStyleProject']")).click();
         getWait2().until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
 
         getWait2().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
 
-        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Project " + name);
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Project " + FREESTYLE_NAME);
+    }
+
+    @Test
+    public void testNameAndDescAreDisplayed(){
+        String desc = "Description";
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("name"))).sendKeys(FREESTYLE_NAME);
+        getDriver().findElement(By.xpath("//li[@class = 'hudson_model_FreeStyleProject']")).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
+
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("description"))).sendKeys(desc);
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Project " + FREESTYLE_NAME);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id = 'description']/div")).getText(), desc);
     }
 }
