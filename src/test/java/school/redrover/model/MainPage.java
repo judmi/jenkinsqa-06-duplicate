68e455ab9515f987e54d7b933a2cb957edadd43c
Sergey.D <sergei.demyanenko@gmail.com>
2023-05-19 00:21:07 -0400

    Add first POM

---
+++ src/test/java/school/redrover/model/MainPage.java
@@ -0,0 +1,11 @@
+package school.redrover.model;
+
+import org.openqa.selenium.WebDriver;
+import school.redrover.model.base.BasePage;
+
+public class MainPage extends BasePage {
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+}
