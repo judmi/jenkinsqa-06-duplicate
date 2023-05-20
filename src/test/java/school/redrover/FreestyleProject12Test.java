0d68c7222dba3b8a445f99c7242bd796c708fafd
Julia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-05-20 22:01:16 +0300

    AT 00.01 Create with an existing name (#2058)

--- src/test/java/school/redrover/FreestyleProject12Test.java
+++ src/test/java/school/redrover/FreestyleProject12Test.java
@@ -6,8 +6,11 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
 public class FreestyleProject12Test extends BaseTest {
+
+    private static final String PROJECT_NAME = "A-freestyle-project";
 
     @DataProvider(name = "specialCharacters")
     public static Object[][] specialCharacters() {
@@ -26,5 +29,19 @@ public class FreestyleProject12Test extends BaseTest {
 
         Assert.assertEquals(unsafeCharMessage.getText(), String.format("» ‘%s’ is an unsafe character", specialCharacter));
         Assert.assertFalse(getDriver().findElement(By.id("ok-button")).isEnabled());
+    }
+
+    @Test
+    public void testCreateWithExistingName() {
+        TestUtils.createFreestyleProject(this, PROJECT_NAME, true);
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        WebElement alreadyExistsMessage = getDriver().findElement(By.xpath("//div[@id='main-panel']/p"));
+
+        Assert.assertEquals(alreadyExistsMessage.getText(),
+                String.format("A job already exists with the name ‘%s’", PROJECT_NAME));
     }
 }
