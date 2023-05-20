0fe0331b9cc4f0139fd1733b3f159a669299ded0
NikolayMkv <i@nikomar.ru>
2023-05-20 21:48:59 +0700

    delets_Locators_Pipeline_Job_Test (#2045)

--- src/test/java/school/redrover/PipelineJobTest.java
+++ src/test/java/school/redrover/PipelineJobTest.java
@@ -8,38 +8,46 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class PipelineJobTest extends BaseTest {
-    private static final By DASHBOARD = By.xpath("//*[@id=\"breadcrumbs\"]/li[1]/a");
-    private static final By NEW_ITEM = By.xpath("//*[@id='tasks']/div[1]/span/a");
-    private static final By ITEM_NAME_ENTER = By.name("name");
-    private static final By OK_BUTTON = By.xpath("//*[@id='ok-button']");
-    private static final By SAVE_BUTTON = By.name("Submit");
-    private static final By PROJECT_NAME = By.xpath("//*[@id=\"job_RedRover\"]/td[3]/a/span");
-    private static final By PIPELINE_PROJECT = By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[2]/div[2]");
-    private static final By BUTTON_ADVANCED = By.xpath("//*[@id='main-panel']/form/div[1]/div[6]/div[2]/div[1]/button");
-    private static final By DISPLAY_NAME = By.xpath("//*[@id='main-panel']/form/div[1]/div[6]/div[3]/div/div[2]/input");
-    private static final By BUILD_NOW_PIPELINE = By.xpath("//*[@id='tasks']/div[3]/span/a");
-    private static final By OPTIONS_DROPDOWN = By.xpath("//*[@id='main-panel']/form/div[1]/div[7]/div[3]/div/div/div[2]/div[2]/div/div[1]/select/option[2]");
 
     private void backToDashboard() {
-        getDriver().findElement(DASHBOARD).click();
+        getDriver().findElement(
+                By.xpath("//*[@id='breadcrumbs']/li[1]/a")
+        ).click();
     }
 
-
     private void CreatePipelineProjectJob(String nameProject, String displayName) {
-        getDriver().findElement(NEW_ITEM).click();
-        getDriver().findElement(ITEM_NAME_ENTER).sendKeys(nameProject);
-        getDriver().findElement(PIPELINE_PROJECT).click();
-        getDriver().findElement(OK_BUTTON).click();
+        getDriver().findElement(
+                By.xpath("//*[@id='tasks']/div[1]/span/a")
+        ).click();
+        getDriver().findElement(
+                By.name("name")
+        ).sendKeys(nameProject);
+        getDriver().findElement(
+                By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[2]/div[2]")
+        ).click();
+        getDriver().findElement(
+                By.xpath("//*[@id='ok-button']")
+        ).click();
 
         new Actions(getDriver())
                 .scrollByAmount(0, 600)
-                .click(getDriver().findElement(BUTTON_ADVANCED))
+                .click(getDriver().findElement(
+                        By.xpath("//*[@id='main-panel']/form/div[1]/div[6]/div[2]/div[1]/button")
+                ))
                 .perform();
-        getWait5().until(ExpectedConditions.elementToBeClickable(DISPLAY_NAME)).sendKeys(displayName);
-        getWait5().until(ExpectedConditions.elementToBeClickable(OPTIONS_DROPDOWN)).click();
-        getWait5().until(ExpectedConditions.elementToBeClickable(SAVE_BUTTON)).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='main-panel']/form/div[1]/div[6]/div[3]/div/div[2]/input")
+        )).sendKeys(displayName);
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='main-panel']/form/div[1]/div[7]/div[3]/div/div/div[2]/div[2]/div/div[1]/select/option[2]")
+        )).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.name("Submit")
+        )).click();
 
-        getWait5().until(ExpectedConditions.elementToBeClickable(BUILD_NOW_PIPELINE)).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='tasks']/div[3]/span/a")
+        )).click();
         backToDashboard();
     }
 
@@ -47,6 +55,8 @@ public class PipelineJobTest extends BaseTest {
     public void testCreatePipelineProjectJob() {
         CreatePipelineProjectJob("RedRover","R&R");
 
-        Assert.assertEquals(getWait10().until(ExpectedConditions.elementToBeClickable(PROJECT_NAME)).getText(),"R&R");
+        Assert.assertEquals(getWait10().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='job_RedRover']/td[3]/a/span"
+                ))).getText(),"R&R");
     }
 }
