533f1d3113ecee9a758ce70d861a02b7395107d5
DaryaSLN <94400517+DaryaSLN@users.noreply.github.com>
2023-05-21 22:03:08 +0300

    AT 12.01 | Dashboard Line > Dashboard dropdown-menu > Select Manage P… (#2082)
    * AT 12.01 | Dashboard Line > Dashboard dropdown-menu > Select Manage Plugins from the drop-down menu from People Page
    
    * AT 12.01 | Dashboard Line > Dashboard dropdown-menu > Select Manage Plugins from the drop-down menu from People Page
    
    * sendKeys after Actions
    
    ---------
    
    Co-authored-by: Darya <darya.slinkova@gmail.com>

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -70,6 +72,31 @@ public class DashboardTest extends BaseTest {
         getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']"))).click();
         getDriver().findElement(By.xpath("//a[@class='model-link'][contains(text(),'Dashboard')]")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='job/One/']")).getText(),"One");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='job/One/']")).getText(), "One");
+    }
+
+    @Test
+    public void testDropDownMenuFromPeoplePage() {
+        getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']")).click();
+
+        WebElement dashboardButton = getDriver()
+                .findElement(By.xpath("//a[text()='Dashboard']"));
+        new Actions(getDriver())
+                .moveToElement(dashboardButton)
+                .perform();
+
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']/button")).sendKeys(Keys.RETURN);
+
+        WebElement manageJenkinsButton = getDriver()
+                .findElement(By.xpath("//a[@class='yuimenuitemlabel yuimenuitemlabel-hassubmenu']"));
+        WebElement managePluginsButton = getDriver().findElement(By.xpath("//*[@id='yui-gen8']/a/span"));
+        new Actions(getDriver())
+                .moveToElement(manageJenkinsButton)
+                .pause(Duration.ofSeconds(1))
+                .moveToElement(managePluginsButton)
+                .click()
+                .perform();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Plugins");
     }
 }
