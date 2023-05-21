b30f58f6d2ed23e71a555afe180587f051d0f8e9
Inna-Iva <115049379+inna-iva@users.noreply.github.com>
2023-05-21 17:43:22 -0400

    RF 00.01 | Create First Freestyle Project through the Create a Job > Delete Unused Import Statements, Add Rows, Change Class Name (#2087)

--- src/test/java/school/redrover/CreateFreestyleProject4Test.java
+++ src/test/java/school/redrover/CreateFreestyleProject4Test.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class BuildTaskOnWelcomePageTest extends BaseTest {
+public class CreateFreestyleProject4Test extends BaseTest {
 
     final String NAME_FIRST_PROJECT = "firstTest";
 
@@ -15,17 +15,24 @@ public class BuildTaskOnWelcomePageTest extends BaseTest {
 
         WebElement createAJobLink = getDriver().findElement(By.xpath("//a[@href='newJob']"));
         createAJobLink.click();
+
         WebElement inputJobName = getDriver().findElement(By.xpath("//input[@class='jenkins-input']"));
         inputJobName.sendKeys(NAME_FIRST_PROJECT);
+
         WebElement pressFreestyleProject = getDriver().findElement(By.xpath("//div[@id='j-add-item-type-standalone-projects']//li[@class='hudson_model_FreeStyleProject']//span[@class='label']"));
         pressFreestyleProject.click();
+
         WebElement pressOkButton = getDriver().findElement(By.xpath("//button[@id='ok-button']"));
         pressOkButton.click();
+
         WebElement pressSubmitButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
         pressSubmitButton.click();
+
         WebElement pressDashboardButton = getDriver().findElement(By.xpath("//li/a[@href='/'][@class='model-link']"));
         pressDashboardButton.click();
-        WebElement createdNameJob = getDriver().findElement(By.xpath("//td/a[@class='jenkins-table__link model-link inside']/span['test']"));
+
+        WebElement createdNameJob = getDriver().findElement(By.xpath(
+                "//td/a[@class='jenkins-table__link model-link inside']/span['test']"));
         String actualJobName = createdNameJob.getText();
 
         Assert.assertEquals(actualJobName, NAME_FIRST_PROJECT);
