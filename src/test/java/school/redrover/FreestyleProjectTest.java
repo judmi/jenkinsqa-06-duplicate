7f79b1b75d7b602f0e6a2d4a64e47dd87136f362
MariaShy <68644507+MariaShy@users.noreply.github.com>
2023-05-21 16:49:43 +0300

    AT 01.01 | Freestyle project > View project page >Project page contains only the name of project without description (#2072)
    Co-authored-by: Maria Shy <24011989@mail.ru>

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -20,6 +20,7 @@ public class FreestyleProjectTest extends BaseTest {
     private static final String FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
     private static final By GO_TO_DASHBOARD_BUTTON = By.linkText("Dashboard");
     private static final String NEW_FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
+    private static final String TEST_NAME = "Test";
 
     @Ignore
     @Test
@@ -406,6 +407,19 @@ public class FreestyleProjectTest extends BaseTest {
         WebElement createdProject = getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']"));
 
         Assert.assertEquals(createdProject.getText(), projectName);
+    }
+
+    @Test(dependsOnMethods={"testCreateFreestyleProject"})
+    public void testFreestyleProjectNameDisplayedWithoutDescription(){
+        getDriver().findElement(By.xpath("//span[normalize-space()='"+ TEST_NAME +"']")).click();
+        getDriver().findElement(By.xpath("//span[normalize-space()='Changes']")).click();
+        getDriver().findElement(By.xpath("//span[normalize-space()='Status']")).click();
+
+        String actualProjectName = getDriver().findElement(By.xpath("//h1")).getText();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("description-link")));
+
+        Assert.assertEquals(actualProjectName, "Project " + TEST_NAME);
+        Assert.assertEquals(getDriver().findElement(By.id("description-link")).getText(),"Add description");
     }
 }
 
