030c580b5dd08b8158ca0ee4eaa41826a7e62a93
Elen T <103669089+Elygan@users.noreply.github.com>
2023-05-22 11:19:21 +0300

    AT 02.05 Pipeline > Delete Pipeline > Delete Pipeline on the dashboard page (#2089)

--- src/test/java/school/redrover/PipelineProject1Test.java
+++ src/test/java/school/redrover/PipelineProject1Test.java
@@ -1,12 +1,17 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
+
+import java.security.Key;
+import java.time.Duration;
 
 public class PipelineProject1Test extends BaseTest {
     private  final String PROJECT_NAME = "Project1";
@@ -30,4 +35,24 @@ public class PipelineProject1Test extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")). getText(),"Pipeline " + newProjectName);
     }
+
+    @Test
+    public void testDeletePipelineProjectOnDashboardProjectPage(){
+        TestUtils.createPipeline(this, PROJECT_NAME,true);
+
+        Actions action = new Actions(getDriver());
+        WebElement projectNameButton=getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td/a[@href='job/"+PROJECT_NAME+"/']")));
+        action.moveToElement(projectNameButton).perform();
+
+        WebElement dropDownMenuButton = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td/a/button")));
+        dropDownMenuButton.sendKeys(Keys.ENTER);
+
+        WebElement deletePipeline = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Delete Pipeline']")));
+        deletePipeline.click();
+
+        getDriver().switchTo().alert().accept();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Welcome to Jenkins!");
+    }
+
 }
