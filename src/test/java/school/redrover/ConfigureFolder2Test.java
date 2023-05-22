cf9120b9da24d2137c22f2baf82455b22894fa54
killspr <99168339+killspr@users.noreply.github.com>
2023-05-22 08:22:09 +0300

    AT 04.04 | Folder > Configure folder with description (#2029)
    https://trello.com/c/bP4j7A5P

--- src/test/java/school/redrover/ConfigureFolder2Test.java
+++ src/test/java/school/redrover/ConfigureFolder2Test.java
@@ -8,17 +8,19 @@ import school.redrover.runner.BaseTest;
 import static org.testng.Assert.assertEquals;
 
 public class ConfigureFolder2Test extends BaseTest {
+    private static final String PROJECT_NAME = "qwerty";
+
 
     private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob'][@class='task-link ']");
-    private static final By PROJECT_NAME = By.xpath("//input[@name='name']");
+    private static final By PROJECT_NAME_FIELD = By.xpath("//input[@name='name']");
     private static final By FOLDER_PROJECT = By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']");
     private static final By CREATE_PROJECT_BUTTON = By.xpath("//button [@id='ok-button']");
     private static final By FOLDER_NAME_FIELD = By.xpath("//input [@name='_.displayNameOrNull']");
     private static final By FOLDER_SUBMIT_BUTTON = By.xpath("//button [@name='Submit']");
 
-    private void CreateFolder(String name){
+    private void createFolder(){
         getDriver().findElement(NEW_ITEM).click();
-        getWait5().until(ExpectedConditions.elementToBeClickable(PROJECT_NAME)).sendKeys(name);
+        getWait5().until(ExpectedConditions.elementToBeClickable(PROJECT_NAME_FIELD)).sendKeys(PROJECT_NAME);
 
         getDriver().findElement(FOLDER_PROJECT).click();
 
@@ -27,14 +29,23 @@ public class ConfigureFolder2Test extends BaseTest {
 
     @Test
     public void testSetDisplayName(){
-        String projectName = "qwerty";
-        String folderName = "folder";
+        createFolder();
+        String FOLDER_NAME = "folder";
 
-        CreateFolder(projectName);
-
-        getDriver().findElement(FOLDER_NAME_FIELD).sendKeys(folderName);
+        getDriver().findElement(FOLDER_NAME_FIELD).sendKeys(FOLDER_NAME);
         getDriver().findElement(FOLDER_SUBMIT_BUTTON).click();
 
-        assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), folderName);
+        assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), FOLDER_NAME);
+    }
+
+    @Test
+    public void testAddDescription(){
+        createFolder();
+        String fodlerDescription = "description of the Folder Project";
+
+        getDriver().findElement(By.name("_.description")).sendKeys(fodlerDescription);
+        getDriver().findElement(FOLDER_SUBMIT_BUTTON).click();
+
+        assertEquals(getDriver().findElement(By.xpath("//div[@id='view-message']")).getText(), fodlerDescription);
     }
 }
