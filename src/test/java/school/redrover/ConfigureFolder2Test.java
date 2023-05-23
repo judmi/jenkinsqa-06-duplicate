2ef32d0df4c09586996f8bd7b9a1ce28b09bbdca
killspr <99168339+killspr@users.noreply.github.com>
2023-05-23 16:59:17 +0300

    AT 04.04 | Folder > Configure Folder with Health metrics (#2146)
    add Health Metric

--- src/test/java/school/redrover/ConfigureFolder2Test.java
+++ src/test/java/school/redrover/ConfigureFolder2Test.java
@@ -7,17 +7,19 @@ import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ConfigureFolder2Test extends BaseTest {
     private static final String PROJECT_NAME = "qwerty";
     private static final By FOLDER_SUBMIT_BUTTON = By.xpath("//button [@name='Submit']");
+    private static final By CONFIGURE_FOLDER_BUTTON = By.xpath("//a [@href='/job/qwerty/configure']");
 
     @Test
     public void testSetDisplayName(){
         String FOLDER_NAME = "folder";
         TestUtils.createFolder(this, PROJECT_NAME, false);
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a [@href='/job/qwerty/configure']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(CONFIGURE_FOLDER_BUTTON)).click();
 
         getDriver().findElement(By.xpath("//input [@name='_.displayNameOrNull']")).sendKeys(FOLDER_NAME);
         getDriver().findElement(FOLDER_SUBMIT_BUTTON).click();
@@ -30,11 +32,26 @@ public class ConfigureFolder2Test extends BaseTest {
         String fodlerDescription = "description of the Folder Project";
         TestUtils.createFolder(this, PROJECT_NAME, false);
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a [@href='/job/qwerty/configure']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(CONFIGURE_FOLDER_BUTTON)).click();
 
         getDriver().findElement(By.name("_.description")).sendKeys(fodlerDescription);
         getDriver().findElement(FOLDER_SUBMIT_BUTTON).click();
 
         assertEquals(getDriver().findElement(By.xpath("//div[@id='view-message']")).getText(), fodlerDescription);
+    }
+
+    @Test
+    public void testAddHealthMetric(){
+        TestUtils.createFolder(this, PROJECT_NAME, false);
+        getWait2().until(ExpectedConditions.elementToBeClickable(CONFIGURE_FOLDER_BUTTON)).click();
+
+        getDriver().findElement(By.xpath("//button [@class='jenkins-button advanced-button advancedButton']")).click();
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button [@id='yui-gen1-button']"))).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='yuimenuitemlabel']"))).click();
+
+        assertTrue(getDriver().findElement(By.xpath("//div[@name='healthMetrics']")).isDisplayed());
+
+        getDriver().findElement(FOLDER_SUBMIT_BUTTON).click();
     }
 }
