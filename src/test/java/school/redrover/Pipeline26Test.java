b17676f4793b714ed6f0e8d35743500d2a29004b
AnnaW <66891449+MalykhAV@users.noreply.github.com>
2023-05-23 10:29:01 -0700

    RF 00.02| New Item > Create Pipeline Project > POM (#2135)

--- src/test/java/school/redrover/Pipeline26Test.java
+++ src/test/java/school/redrover/Pipeline26Test.java
@@ -1,9 +1,9 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.MainPage;
 import school.redrover.runner.BaseTest;
 
 public class Pipeline26Test extends BaseTest{
@@ -11,18 +11,14 @@ public class Pipeline26Test extends BaseTest{
     @Test
     public void testCreate() {
 
-        getDriver().findElement(By.xpath("//div[@class='task '][1]")).click();
+        WebElement pipelineElement = new MainPage(getDriver())
+            .clickNewItem()
+            .enterItemName(NAME_PIPELINE_PROJECT)
+            .selectPipelineAndOk()
+            .clickSaveButton()
+            .clickDashboard()
+            .getProjectName();
 
-        WebElement nameField = getDriver().findElement(By.xpath("//input[@id='name']"));
-        nameField.sendKeys(NAME_PIPELINE_PROJECT);
-
-        getDriver().findElement(By.xpath("//*[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
-
-        getDriver().findElement(By.id("ok-button")).click();
-
-        getDriver().findElement(By.name("Submit")).click();
-
-        WebElement pipelineElement = getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']"));
-        Assert.assertEquals(pipelineElement.getText(), "Pipeline " + NAME_PIPELINE_PROJECT);
+        Assert.assertEquals(pipelineElement.getText(), NAME_PIPELINE_PROJECT);
     }
 }
