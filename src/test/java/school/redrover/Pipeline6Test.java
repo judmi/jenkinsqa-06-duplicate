143bf8dc6ba833c57f63ee58436501dcc75772cf
Anna <anna85ek@gmail.com>
2023-05-24 10:44:40 -0400

    RF 00.02| New Item > Create Pipeline Project (#2187)

--- src/test/java/school/redrover/Pipeline6Test.java
+++ src/test/java/school/redrover/Pipeline6Test.java
@@ -1,27 +1,29 @@
 package school.redrover;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.MainPage;
 import school.redrover.runner.BaseTest;
 
 public class Pipeline6Test extends BaseTest {
 
-    final String name = "MyFirstJenkinsProject";
-
+    private static final String NAME_PROJECT = RandomStringUtils.randomAlphanumeric(10);
 
     @Test
     public void testCreatePipelineProject() {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Create a job')]")).click();
 
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(name);
-        getDriver().findElement(By.xpath("//span[normalize-space()='Pipeline']")).click();
+        WebElement pipeline = new MainPage(getDriver())
+                .clickNewItem()
+                .enterItemName(NAME_PROJECT)
+                .selectPipelineAndOk()
+                .clickSaveButton()
+                .clickDashboard()
+                .getProjectName();
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.id("ok-button")))).click();
-
-        getDriver().findElement(By.xpath("//button[contains(text(),'Save')]")).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(text(),'Pipeline " + name + "')]")).getText(), "Pipeline " + name);
+        Assert.assertEquals(pipeline.getText(), NAME_PROJECT);
     }
 }
