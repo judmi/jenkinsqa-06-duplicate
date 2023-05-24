7e0602aeee3d5831cc3d118b9c5f30759ce60c1d
Roman Ivanov <atom10121998@mail.ru>
2023-05-24 22:52:51 +0300

    AT 04.07 | Folder > Create Pipeline Project without description in Folder (#2192)

--- src/test/java/school/redrover/Folder8Test.java
+++ src/test/java/school/redrover/Folder8Test.java
@@ -1,11 +1,14 @@
 package school.redrover;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
+import java.util.List;
 
 public class Folder8Test extends BaseTest {
 
@@ -25,4 +28,39 @@ public class Folder8Test extends BaseTest {
         WebElement titleName = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1")));
         Assert.assertEquals(titleName.getText(), expectedFolderName);
     }
+
+    @Test
+        public void testCreatePipelineProjectWithoutDescriptionInFolder() {
+            final String folderName = RandomStringUtils.randomAlphanumeric(8);
+            final String pipelineName = RandomStringUtils.randomAlphanumeric(8);
+            boolean isPipelinePresent = false;
+
+            TestUtils.createFolder(this, folderName, false);
+
+            getWait5().until(ExpectedConditions.elementToBeClickable(By.linkText("New Item"))).click();
+
+            getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("name"))).sendKeys(pipelineName);
+            getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
+            getDriver().findElement(By.id("ok-button")).click();
+
+            getWait2().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
+
+            String actualPipelineName = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1"))).getText();
+            getDriver().findElement(By.linkText("Dashboard")).click();
+
+            getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='projectstatus']"))).findElement(By.linkText(folderName)).click();
+
+            getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='projectstatus']")));
+            List<WebElement> jobs = getDriver().findElements(By.xpath("//table[@id='projectstatus']/tbody/tr"));
+            for (WebElement job : jobs) {
+                String jobName = job.getText();
+                if ( jobName.contains(pipelineName) ) {
+                    isPipelinePresent = true;
+                    break;
+                }
+            }
+
+            Assert.assertEquals(actualPipelineName, "Pipeline " + pipelineName);
+            Assert.assertTrue(isPipelinePresent);
+        }
 }
