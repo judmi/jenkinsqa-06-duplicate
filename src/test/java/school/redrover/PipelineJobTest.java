86e0e1cbcbd4342aaf4b8f04dc9081c24f0a620e
NikolayMkv <i@nikomar.ru>
2023-05-24 09:32:40 +0700

    RF_PipelineJobReplacementSelector (#2159)

--- src/test/java/school/redrover/PipelineJobTest.java
+++ src/test/java/school/redrover/PipelineJobTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -15,8 +14,8 @@ public class PipelineJobTest extends BaseTest {
                 By.xpath("//*[@id='breadcrumbs']/li[1]/a")
         ).click();
     }
-    @Ignore
-    private void CreatePipelineProjectJob(String nameProject, String displayName) {
+
+    private void createPipelineProjectJob(String nameProject, String displayName) {
         getDriver().findElement(
                 By.xpath("//*[@id='tasks']/div[1]/span/a")
         ).click();
@@ -31,16 +30,21 @@ public class PipelineJobTest extends BaseTest {
         ).click();
 
         new Actions(getDriver())
-                .scrollByAmount(0, 600)
+                .scrollByAmount(0, 800)
                 .click(getDriver().findElement(
                         By.xpath("//*[@id='main-panel']/form/div[1]/div[6]/div[2]/div[1]/button")
                 ))
                 .perform();
+
+        new Actions(getDriver())
+                .click( getWait5().until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("//*[@id='main-panel']//div[1]/div[6]/div[3]/div/div[2]")
+                )))
+                .sendKeys(displayName)
+                .perform();
+
         getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//*[@id='main-panel']/form/div[1]/div[6]/div[3]/div/div[2]/input")
-        )).sendKeys(displayName);
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//*[@id='main-panel']/form/div[1]/div[7]/div[3]/div/div/div[2]/div[2]/div/div[1]/select/option[2]")
+                By.xpath("//*[@id='main-panel']//div[1]/div[7]/div[3]/div/div/div[2]/div[2]/div/div[1]/select/option[2]")
         )).click();
         getWait5().until(ExpectedConditions.elementToBeClickable(
                 By.name("Submit")
@@ -54,7 +58,7 @@ public class PipelineJobTest extends BaseTest {
 
     @Test
     public void testCreatePipelineProjectJob() {
-        CreatePipelineProjectJob("RedRover","R&R");
+        createPipelineProjectJob("RedRover","R&R");
 
         Assert.assertEquals(getWait10().until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//*[@id='job_RedRover']/td[3]/a/span"
