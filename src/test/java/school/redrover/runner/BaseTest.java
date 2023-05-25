5138e9edeb0a4d6468d3ba692cd50fefea7c56d4
Sergey.D <sergei.demyanenko@gmail.com>
2023-05-24 22:15:40 -0400

    Add several attempts at browser startup

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -70,7 +70,19 @@ public abstract class BaseTest {
 
     protected void startDriver() {
         ProjectUtils.log("Browser open");
-        driver = ProjectUtils.createDriver();
+
+        int count = 0;
+        do {
+            try {
+                Thread.sleep(500);
+                driver = ProjectUtils.createDriver();
+            } catch (Exception e) {
+                if (++count >= 3) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } while (driver == null);
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
