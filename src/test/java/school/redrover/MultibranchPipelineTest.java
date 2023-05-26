0175200726d49c2b4cc162d71609f9a40c1198e6
Anna <anna85ek@gmail.com>
2023-05-25 22:49:34 -0400

    RF test Create Multibranch Pipeline With out Description (#2251)

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -43,6 +43,17 @@ public class MultibranchPipelineTest extends BaseTest {
         Assert.assertTrue(new MultibranchPipelinePage(getDriver()).multibranchPipeline().getText().contains("MultibranchPipeline"));
     }
     @Test
+    public void testCreateMultibranchPipelineWithoutDescription() {
+        MultibranchPipelinePage pageWithOutDescription = new MainPage(getDriver())
+                .clickNewItem()
+                .enterItemName("MineMultibranchPipelineWhitOutDescription")
+                .selectMultibranchPipelineAndOk()
+                .displayName("Random name")
+                .saveButton();
+
+        Assert.assertTrue(new MultibranchPipelineConfigPage(getDriver()).viewDescription().getText().isEmpty());
+    }
+    @Test
     public void deleteMultibranchPipelineTest() {
         new MainPage(getDriver())
                 .clickNewItem()
