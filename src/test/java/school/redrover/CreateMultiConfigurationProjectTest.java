4dc534eace7b1d0df3ae5e647589824526492f0f
Maria Bass <bassmari@gmail.com>
2023-05-25 16:21:45 -0800

    Combining Multi-Configuration project tests to one class - 3d commit.

---
+++ src/test/java/school/redrover/CreateMultiConfigurationProjectTest.java
@@ -0,0 +1,23 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateMultiConfigurationProjectTest extends BaseTest {
+
+
+
+
+
+}
