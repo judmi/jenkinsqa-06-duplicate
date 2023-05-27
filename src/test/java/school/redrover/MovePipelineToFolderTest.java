e4390aca90e72bcb7c23be7959efa3b538c67bc6
Kyrylo K <130118481+Kk-788@users.noreply.github.com>
2023-05-27 00:53:33 -0700

    Add MovePipelineToFolderTest containing testMovePipelineToFolder() (#2275)
    AT 04.13 | Folder > Move Pipeline Project to Folder > MovePipelineToFolderTest

---
+++ src/test/java/school/redrover/MovePipelineToFolderTest.java
@@ -0,0 +1,36 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import school.redrover.model.MainPage;
+import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class MovePipelineToFolderTest extends BaseTest {
+
+    @Test
+    public void testMovePipelineToFolder() {
+
+        TestUtils.createFolder(this, "testFolder",true);
+        TestUtils.createPipeline(this, "testPipeline",true);
+
+        new MainPage(getDriver())
+        .clickJobDropDownMenu("testPipeline")
+        .selectMoveJobDropDownMenu("testPipeline")
+        .selectDestinationFolder()
+        .clickMoveButton()
+        .navigateToMainPageByBreadcrumbs()
+        .clickFolderName("testFolder")
+        .getNestedFolder("testPipeline")
+        .click();
+
+        WebElement breadcrumbBar = getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']"));
+        String breadcrumbText = breadcrumbBar.getText().replaceAll("\\W"," > ");
+        assertEquals(breadcrumbText, "Dashboard > testFolder > testPipeline");
+
+    }
+
+}
