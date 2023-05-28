2e04e2ef16fb33155b51098b55f75cae38d4bebe
Mariia Rykova <129385242+RykovaMariia@users.noreply.github.com>
2023-05-28 17:53:39 +0400

    RF | POM > Refactoring TestUtils (#2306)

--- src/test/java/school/redrover/runner/TestUtils.java
+++ src/test/java/school/redrover/runner/TestUtils.java
@@ -1,62 +1,85 @@
 package school.redrover.runner;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
+import school.redrover.model.JobPage;
+import school.redrover.model.MainPage;
+import school.redrover.model.NewJobPage;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestUtils {
 
     private static void createProject(BaseTest baseTest, String name) {
-        baseTest.getDriver().findElement(By.linkText("New Item")).click();
-        baseTest.getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='name']"))).sendKeys(name);
+        new MainPage(baseTest.getDriver())
+                .clickNewItem()
+                .enterItemName(name);
     }
 
-    private static void saveProject(BaseTest baseTest, Boolean goToHomePage) {
-        baseTest.getWait2().until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
-        baseTest.getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='Submit']"))).click();
-
+    private static void goToHomePage(BaseTest baseTest, Boolean goToHomePage) {
         if (goToHomePage) {
-            baseTest.getDriver().findElement(By.linkText("Dashboard")).click();
+            new JobPage(baseTest.getDriver())
+                    .clickDashBoardButton();
         }
     }
 
     public static void createFreestyleProject(BaseTest baseTest, String name, Boolean goToHomePage) {
         createProject(baseTest, name);
-        baseTest.getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
-        saveProject(baseTest, goToHomePage);
+
+        new NewJobPage(baseTest.getDriver())
+                .selectFreestyleProjectAndOk()
+                .clickSave();
+
+        goToHomePage(baseTest, goToHomePage);
     }
 
     public static void createPipeline(BaseTest baseTest, String name, Boolean goToHomePage) {
         createProject(baseTest, name);
-        baseTest.getDriver().findElement(By.xpath("//label/span[text()='Pipeline']")).click();
-        saveProject(baseTest, goToHomePage);
+
+        new NewJobPage(baseTest.getDriver())
+                .selectPipelineAndOk()
+                .clickSaveButton();
+
+        goToHomePage(baseTest, goToHomePage);
     }
 
     public static void createMultiConfigurationProject(BaseTest baseTest, String name, Boolean goToHomePage) {
         createProject(baseTest, name);
-        baseTest.getDriver().findElement(By.xpath("//label/span[contains(text(), 'Multi-configuration proj')]")).click();
-        saveProject(baseTest, goToHomePage);
+
+        new NewJobPage(baseTest.getDriver())
+                .selectMultiConfigurationProjectAndOk()
+                .saveConfigurePageAndGoToProjectPage();
+
+        goToHomePage(baseTest, goToHomePage);
     }
 
     public static void createFolder(BaseTest baseTest, String name, Boolean goToHomePage) {
         createProject(baseTest, name);
-        baseTest.getDriver().findElement(By.cssSelector(".com_cloudbees_hudson_plugins_folder_Folder")).click();
-        saveProject(baseTest, goToHomePage);
+
+        new NewJobPage(baseTest.getDriver())
+                .selectFolderAndOk()
+                .clickSaveButton();
+
+        goToHomePage(baseTest, goToHomePage);
     }
 
     public static void createMultibranchPipeline(BaseTest baseTest, String name, Boolean goToHomePage) {
         createProject(baseTest, name);
-        baseTest.getDriver().findElement(By.xpath("//span[text()='Multibranch Pipeline']")).click();
-        saveProject(baseTest, goToHomePage);
+
+        new NewJobPage(baseTest.getDriver())
+                .selectMultibranchPipelineAndOk()
+                .saveButton();
+
+        goToHomePage(baseTest, goToHomePage);
     }
 
     public static void createOrganizationFolder(BaseTest baseTest, String name, Boolean goToHomePage) {
         createProject(baseTest, name);
-        baseTest.getDriver().findElement(By.xpath("//label/span[contains(text(), 'Organization Folder')]")).click();
-        saveProject(baseTest, goToHomePage);
+
+        new NewJobPage(baseTest.getDriver())
+                .selectOrganizationFolderAndOk()
+                .clickSaveButton();
+
+        goToHomePage(baseTest, goToHomePage);
     }
 
     public static List<String> getTexts(List<WebElement> elements) {
