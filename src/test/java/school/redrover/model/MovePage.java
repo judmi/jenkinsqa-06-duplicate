32f330d515650d5fccaad53bedaa4004b8ef1079
Mariia Rykova <129385242+RykovaMariia@users.noreply.github.com>
2023-05-30 19:34:34 +0400

    RF | MovePage, RenamePage >generics refactoring (#2346)

--- src/test/java/school/redrover/model/MovePage.java
+++ src/test/java/school/redrover/model/MovePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import school.redrover.model.base.BaseMainHeaderPage;
 import school.redrover.model.base.BasePage;
 
-public class MovePage<JobTypePage extends BasePage<?>> extends BaseMainHeaderPage {
+public class MovePage<JobTypePage extends BasePage<?>> extends BaseMainHeaderPage<MovePage<JobTypePage>> {
 
     private final JobTypePage jobTypePage;
 
--- src/test/java/school/redrover/model/RenamePage.java
+++ src/test/java/school/redrover/model/RenamePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 import school.redrover.model.base.BasePage;
 
-public class RenamePage <JobTypePage extends BasePage<?>> extends BaseMainHeaderPage {
+public class RenamePage <JobTypePage extends BasePage<?>> extends BaseMainHeaderPage<RenamePage<JobTypePage>> {
 
     private final JobTypePage jobTypePage;
 
