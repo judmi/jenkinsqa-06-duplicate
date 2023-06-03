53161cc708594a6be592e79025e048caafa8f6ad
DaryaSLN <94400517+DaryaSLN@users.noreply.github.com>
2023-06-03 22:21:24 +0300

    RF 12.01 |  POM > Select Manage Plugins from the drop-down menu from People Page (#2422)
    Co-authored-by: Darya <darya.slinkova@gmail.com>

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -1,16 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.model.MainPage;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
 
@@ -57,27 +53,13 @@ public class DashboardTest extends BaseTest {
     }
 
     @Test
-    public void testDropDownMenuFromPeoplePage() {
-        getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']")).click();
+    public void testMoveFromPeoplePageToPluginsPageByDropDownMenu() {
+        String actualTitle = new MainPage(getDriver())
+                .clickPeopleOnLeftSideMenu()
+                .getHeader()
+                .openPluginsPageFromDashboardDropdownMenu()
+                .getPageTitle();
 
-        WebElement dashboardButton = getDriver()
-                .findElement(By.xpath("//a[text()='Dashboard']"));
-        new Actions(getDriver())
-                .moveToElement(dashboardButton)
-                .perform();
-
-        getDriver().findElement(By.xpath("//a[text()='Dashboard']/button")).sendKeys(Keys.RETURN);
-
-        WebElement manageJenkinsButton = getDriver()
-                .findElement(By.xpath("//a[@class='yuimenuitemlabel yuimenuitemlabel-hassubmenu']"));
-        WebElement managePluginsButton = getDriver().findElement(By.xpath("//*[@id='yui-gen8']/a/span"));
-        new Actions(getDriver())
-                .moveToElement(manageJenkinsButton)
-                .pause(Duration.ofSeconds(1))
-                .moveToElement(managePluginsButton)
-                .click()
-                .perform();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Plugins");
+        Assert.assertEquals(actualTitle, "Plugins");
     }
 }
