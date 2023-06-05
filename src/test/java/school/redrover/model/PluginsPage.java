4c020221c1d8eba04d81fa7fd1580883c18194a7
Maiia Pivovarova <pivovarovamv@gmail.com>
2023-06-06 01:02:07 +0700

    https://trello.com/c/bfVKVhLh

--- src/test/java/school/redrover/model/PluginsPage.java
+++ src/test/java/school/redrover/model/PluginsPage.java
@@ -2,8 +2,6 @@ package school.redrover.model;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class PluginsPage extends BaseMainHeaderPage<PluginsPage> {
