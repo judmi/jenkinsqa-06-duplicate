18bd9730b2595a1f3f1bc66f49d59e8e45d1fcd7
Maiia Pivovarova <pivovarovamv@gmail.com>
2023-06-06 00:16:26 +0700

    https://trello.com/c/bfVKVhLh

--- src/test/java/school/redrover/ManagePluginsTest.java
+++ src/test/java/school/redrover/ManagePluginsTest.java
@@ -1,5 +1,6 @@
 package school.redrover;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.MainPage;
 import school.redrover.runner.BaseTest;
@@ -7,7 +8,6 @@ import school.redrover.runner.BaseTest;
 import java.util.List;
 
 public class ManagePluginsTest extends BaseTest {
-
     @Test
     public void testFourTasksOnLeftsidePanel() {
         final List<String> expectedListOfTasks = List.of(new String[]{"Updates", "Available plugins", "Installed plugins", "Advanced settings"});
@@ -18,4 +18,19 @@ public class ManagePluginsTest extends BaseTest {
 
         Assert.assertEquals(actualListOfTasks, expectedListOfTasks);
     }
+
+    @Test
+    public void testServerInfoBoxAdvancedSettingsPluginPage(){
+        final String expectedInfoServerText = "If your Jenkins server sits behind a firewall ";
+        String ServerInfoBox = new MainPage(getDriver())
+                .navigateToManageJenkinsPage()
+                .clickManagePlugins()
+                .clickAdvancedSettings()
+                .clickExtraInfoServerIcon()
+                .getExtraInfoServerTextBox();
+        Assert.assertTrue(ServerInfoBox.contains(expectedInfoServerText));
+
+
+    }
+
 }
