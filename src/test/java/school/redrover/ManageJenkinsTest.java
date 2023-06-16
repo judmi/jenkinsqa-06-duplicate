e1bce7d75ae674b3888cf3ae69d16539b2ac8252
Ruslan Fursa <fursa_1986@mail.ru>
2023-06-17 00:04:21 +0300

    add testCreateNewUser

---
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -0,0 +1,25 @@
+package school.redrover;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.model.MainPage;
+import school.redrover.runner.BaseTest;
+
+public class ManageJenkinsTest extends BaseTest {
+    @Test
+    public void testCreateNewUser() {
+        String userName = new Faker().name().firstName();
+        String password = new Faker().internet().password();
+        String email = new Faker().internet().emailAddress();
+        boolean isUserCreated = new MainPage(getDriver())
+                .clickManageJenkinsTab()
+                .clickManageUsersSection()
+                .clickCrateUserBtn()
+                .fillInCredentialsAndSubmit(userName, password, email)
+                .isUserExist(userName);
+        Assert.assertTrue(isUserCreated);
+
+
+    }
+}
--- src/test/java/school/redrover/model/MainPage.java
+++ src/test/java/school/redrover/model/MainPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import school.redrover.model.base.BasePage;
 
@@ -8,4 +9,10 @@ public class MainPage extends BasePage {
     public MainPage(WebDriver driver) {
         super(driver);
     }
+
+    public ManageJenkinsPage clickManageJenkinsTab() {
+        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
+        return new ManageJenkinsPage(getDriver());
+    }
 }
+
