905cb37cfcff3ac16e7e5d0f08ba1b95eaa9ed64
Natalya Kadysheva <Natalya.Kadysheva@nedra.digital>
2023-06-19 11:36:54 +0300

    Add Enum JobType in TestUtils class! Make an empty strings after Find.by!

--- src/test/java/school/redrover/model/MainPage.java
+++ src/test/java/school/redrover/model/MainPage.java
@@ -1,6 +1,5 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
