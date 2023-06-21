f0e65269eb1512194e56db32c0a51677c72e53a5
Natalya Kadysheva <Natalya.Kadysheva@nedra.digital>
2023-06-21 23:01:32 +0300

    Add MulticonfigurationProjectConfigPage, fix methods.

---
+++ src/test/java/school/redrover/model/MulticonfigurationProjectConfigPage.java
@@ -0,0 +1,10 @@
+package school.redrover.model;
+
+import org.openqa.selenium.WebDriver;
+import school.redrover.model.base.BasePage;
+
+public class MulticonfigurationProjectConfigPage extends BasePage {
+    public MulticonfigurationProjectConfigPage(WebDriver driver) {
+        super(driver);
+    }
+}
